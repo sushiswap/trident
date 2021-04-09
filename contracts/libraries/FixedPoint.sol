82ad73f6ac7e38f0ff8fcdf0e526118f537011e6
SolusLumen <74754077+SolusLumen@users.noreply.github.com>
2021-04-09 15:00:37 +0900

    Add test code for libraries
    * FixedPoint
    * SafeERC20
    * EnumerableMap
    * EnumerableSet
    * MathUtils

--- contracts/libraries/FixedPoint.sol
+++ contracts/libraries/FixedPoint.sol
@@ -40,9 +40,7 @@ library FixedPoint {
     // multiply a UQ112x112 by a uint, returning a UQ144x112
     // reverts on overflow
     function mul(uq112x112 memory self, uint256 y) internal pure returns (uq144x112 memory) {
-        uint256 z;
-        require(y == 0 || (z = uint256(self._x) * y) / y == uint256(self._x), "FixedPoint: MULTIPLICATION_OVERFLOW");
-        return uq144x112(z);
+        return uq144x112(uint256(self._x) * y);
     }
 
     // divide a UQ144x112 by a uint112, returning a UQ144x112
@@ -54,9 +52,7 @@ library FixedPoint {
     // multiply a UQ144x112 by a uint, returning a UQ144x112
     // reverts on overflow
     function mul(uq144x112 memory self, uint256 y) internal pure returns (uq144x112 memory) {
-        uint256 z;
-        require(y == 0 || (z = self._x * y) / y == self._x, "FixedPoint: MULTIPLICATION_OVERFLOW");
-        return uq144x112(z);
+        return uq144x112(self._x * y);
     }
 
     // returns a UQ112x112 which represents the ratio of the numerator to the denominator
---
+++ contracts/test/ERC20Token1.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.2;
+
+import "../pool/MirinERC20.sol";
+
+contract ERC20Token is MirinERC20 {
+    
+    constructor() {
+        mint(msg.sender, 100000);
+    }
+    
+    function mint(address _to, uint256 _amount) public {
+        _mint(_to, _amount);
+    }
+}
