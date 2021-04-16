fe35c00e1f01b8cacde335096e8c62817ea93dbf
Jongseung Lim <penandlim@gmail.com>
2021-04-16 03:28:13 -0400

    Add solhint dependency for linting

--- test/MirinMath.test.js
+++ test/MirinMath.test.js
@@ -6,14 +6,14 @@ describe("MirinMath Test", function () {
     let Test, test;
     let i = 0;
     let j, k, con, js, temp;
-    
+
     const Fixed1 = BigNumber.from(2).pow(127);
     const Fixed2 = BigNumber.from(2).pow(128);
     const Fixed3 = BigNumber.from(2).pow(97);
     const Fixed4 = BigNumber.from(2).pow(240);
     const Fixed5 = BigNumber.from(2).pow(113);
     const Base = BigNumber.from(10).pow(8);
-    
+
     function getBaseLog(x, y) {
         return Math.log(y) / Math.log(x);
     }
@@ -27,10 +27,10 @@ describe("MirinMath Test", function () {
         con = await test._findPositionInMaxExpArray(0);
         expect(con).to.be.equal(127);
         await expect(test._findPositionInMaxExpArray(1)).to.be.reverted;
-        
+
         await test._initialize();
         expect(await test._findPositionInMaxExpArray(1)).to.be.equal(127);
-        
+
         temp = BigNumber.from("0x0292c5bdd3b92ec810287b1b3fffffffff").sub(100); //maxExpArray[89] = 0x0292c5bdd3b92ec810287b1b3fffffffff
         expect(await test._findPositionInMaxExpArray(temp)).to.be.equal(89);
 
@@ -136,10 +136,13 @@ describe("MirinMath Test", function () {
         await expect(test._optimalLog(Fixed1.sub(1))).to.be.revertedWith("MIRIN: Outranged");
         await expect(test._optimalLog(Fixed4)).to.be.reverted;
         i = 0;
-        while(i < 10) {
-            j = Math.floor(((Math.random() * (Math.exp(1) - 1)) + 1) * Base);
+        while (i < 10) {
+            j = Math.floor((Math.random() * (Math.exp(1) - 1) + 1) * Base);
             k = Fixed1.mul(j).div(Base);
-            con = BigNumber.from(await test._optimalLog(k)).mul(Base).div(Fixed1).toNumber();
+            con = BigNumber.from(await test._optimalLog(k))
+                .mul(Base)
+                .div(Fixed1)
+                .toNumber();
             js = Math.floor(Math.log(k / Fixed1) * Base);
             assert.isTrue(Math.abs(con - js) / con < Math.pow(10, -6));
             i++;
@@ -152,21 +155,23 @@ describe("MirinMath Test", function () {
             j = Math.ceil(Math.random() * Math.pow(2, 30)); //(0.xxx ~ 1.000) * 2^30
             k = Fixed3.mul(j); //(0.xxx ~ 1.000) * 2^127
             con = BigNumber.from(await test._optimalExp(k))
-            .div(Fixed1)
-            .toNumber();
+                .div(Fixed1)
+                .toNumber();
             js = Math.floor(Math.exp(j / Math.pow(2, 30)));
             assert.isTrue(con === js);
-            
+
             j = Math.floor(Math.random() * 16 * Math.pow(2, 30)); //(0.xxx ~ 15.xxx) * 2^30
             k = Fixed3.mul(j); //(0.xxx ~ 15.xxx) * 2^127
             con = BigNumber.from(await test._optimalExp(k))
-            .div(Fixed1)
-            .toNumber();
+                .div(Fixed1)
+                .toNumber();
             js = Math.floor(Math.exp(j / Math.pow(2, 30)));
             assert.isTrue(con === js);
             i++;
         }
-        await expect(test._optimalExp(BigNumber.from("0x800000000000000000000000000000000"))).to.be.revertedWith("MIRIN: Outranged");
+        await expect(test._optimalExp(BigNumber.from("0x800000000000000000000000000000000"))).to.be.revertedWith(
+            "MIRIN: Outranged"
+        );
     });
 
     //power            TODO
