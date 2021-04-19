c06009e6895f71a5d26c71c7dc1ee57452923783
SolusLumen <kavanaboy@gmail.com>
2021-04-19 22:16:35 +0900

    Add MirinMath2, Change computeAmountIn/Out

--- contracts/pool/curves/ConstantMeanCurve.sol
+++ contracts/pool/curves/ConstantMeanCurve.sol
@@ -5,6 +5,7 @@ pragma solidity =0.8.2;
 import "../../MirinMath.sol";
 import "../../interfaces/IMirinCurve.sol";
 import "../../libraries/FixedPoint.sol";
+import "../../libraries/MirinMath2.sol";
 
 /**
  * @dev Constant mean curve for tokens with different possible weights (k = r_0^w_0 * r_1^w1)
@@ -12,6 +13,7 @@ import "../../libraries/FixedPoint.sol";
  */
 contract ConstantMeanCurve is IMirinCurve, MirinMath {
     using FixedPoint for *;
+    using MirinMath2 for uint256;
 
     uint8 public constant MAX_SWAP_FEE = 100;
     uint8 public constant WEIGHT_SUM = 100;
@@ -60,9 +62,12 @@ contract ConstantMeanCurve is IMirinCurve, MirinMath {
         require(swapFee < MAX_SWAP_FEE, "MIRIN: INVALID_SWAP_FEE");
         (uint112 reserveIn, uint112 reserveOut) = tokenIn == 0 ? (reserve0, reserve1) : (reserve1, reserve0);
         (uint8 weightIn, uint8 weightOut) = decodeData(data, tokenIn);
-        (uint256 numerator, ) = power(reserveIn, 1, weightIn, weightOut);
-        (uint256 denominator, ) = power(reserveIn + (amountIn * (1000 - swapFee)) / 1000, 1, weightIn, weightOut);
-        amountOut = reserveOut - (reserveOut * numerator) / denominator;
+
+        uint256 weightRatio = MirinMath2.roundDiv(uint256(weightIn), uint256(weightOut));
+        uint256 adjustedIn = MirinMath2.roundMul(amountIn, MirinMath2.BASE - (uint256(swapFee) * 10**15));
+        uint256 base = MirinMath2.roundDiv(uint256(reserveIn), uint256(reserveIn) + adjustedIn);
+        uint256 pow = MirinMath2.power(base, weightRatio);
+        amountOut = MirinMath2.roundMul(uint256(reserveOut), MirinMath2.BASE - pow);
     }
 
     function computeAmountIn(
@@ -76,10 +81,13 @@ contract ConstantMeanCurve is IMirinCurve, MirinMath {
         require(amountOut > 0, "MIRIN: INSUFFICIENT_INPUT_AMOUNT");
         require(reserve0 > 0 && reserve0 > 0, "MIRIN: INSUFFICIENT_LIQUIDITY");
         require(swapFee < MAX_SWAP_FEE, "MIRIN: INVALID_SWAP_FEE");
-        (uint112 reserveIn, uint112 reserveOut) = tokenIn == 0 ? (reserve0, reserve1) : (reserve1, reserve0);
         (uint8 weightIn, uint8 weightOut) = decodeData(data, tokenIn);
-        (uint256 result, uint8 precision) = power(reserveOut, reserveOut - amountOut, weightOut, weightIn);
-        amountIn = (((reserveIn * result) / (1 << precision) - reserveIn) * 1000) / (1000 - swapFee);
+        (uint112 reserveIn, uint112 reserveOut) = tokenIn == 0 ? (reserve0, reserve1) : (reserve1, reserve0);
+
+        uint256 weightRatio = MirinMath2.roundDiv(uint256(weightOut), uint256(weightIn));
+        uint256 base = MirinMath2.roundDiv(uint256(reserveOut), uint256(reserveOut) - amountOut);
+        uint256 pow = MirinMath2.power(base, weightRatio);
+        amountIn = uint256(reserveIn) * (pow - MirinMath2.BASE) / MirinMath2.BASE - (uint256(swapFee) * 10**15);
     }
 
     function decodeData(bytes32 data, uint8 tokenIn) public pure returns (uint8 weightIn, uint8 weightOut) {
