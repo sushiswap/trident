d79416e17e55534edb5e5f8a7d347e616ea8159e
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-06-06 20:30:15 -0400

    Create IMirinCallee.sol

---
+++ contracts/interfaces/IMirinCallee.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+interface IMirinCallee {
+    function mirinCall(address sender, uint256 amount0, uint256 amount1, bytes calldata data) external;
+}
