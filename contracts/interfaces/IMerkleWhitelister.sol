49bb95c69f2462022fb788c1122c9b9f021b45c7
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-06-21 11:05:58 -0400

    Update IMerkleWhitelister.sol
    add operator function event

--- contracts/interfaces/IMerkleWhitelister.sol
+++ contracts/interfaces/IMerkleWhitelister.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.8.2;
 
 /// @notice Allows anyone to join SushiSwap pool whitelist if they exist in a merkle root.
 interface IMerkleWhitelister {
-    // Returns the merkle root of the merkle tree containing accounts available to join list.
+    /// @dev Returns the merkle root of the merkle tree containing accounts available to join whitelist.
     function merkleRoot() external view returns (bytes32);
-    // Returns true if the index has been marked claimed.
+    /// @dev Returns true if the index has been marked claimed.
     function isWhitelisted(uint256 index) external view returns (bool);
-    // Claim the given amount of the token to the given address. Reverts if the inputs are invalid.
+    /// @dev Claim spot in whitelist for an account. Reverts if the inputs are invalid.
     function joinWhitelist(uint256 index, address account, bytes32[] calldata merkleProof) external;
-
-    // This event is triggered whenever a {joinWhitelist} call succeeds.
-    event Whitelisted(uint256 index, address account);
+    /// @dev This event is triggered whenever a {joinWhitelist} call succeeds.
+    event JoinWhitelist(uint256 index, address account);
+    /// @dev This event is triggered whenever a {setMerkleRoot} call succeeds.
+    event SetMerkleRoot(bytes32 merkleRoot); 
 }
