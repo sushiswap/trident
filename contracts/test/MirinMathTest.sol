0b907190aa5c219cb7fd85c30b5e78f1b4896df9
nori <nori@sushi.com>
2021-07-16 21:55:13 +0200

    MirinMath: Cleanup and Tests
    Removed all unused constants and functions
    
    only sqrt() is left

--- contracts/test/MirinMathTest.sol
+++ contracts/test/MirinMathTest.sol
@@ -5,23 +5,7 @@ pragma solidity ^0.8.2;
 import "../libraries/MirinMath.sol";
 
 contract MirinMathTest {
-    function _floorLog2(uint256 _n) public pure returns (uint8) {
-        return MirinMath.floorLog2(_n);
-    }
-
-    function _ln(uint256 x) public pure returns (uint256) {
-        return MirinMath.ln(x);
-    }
-
-    function _generalLog(uint256 x) public pure returns (uint256) {
-        return MirinMath.generalLog(x);
-    }
-
-    function _optimalLog(uint256 x) public pure returns (uint256) {
-        return MirinMath.optimalLog(x);
-    }
-
-    function _optimalExp(uint256 x) public pure returns (uint256) {
-        return MirinMath.optimalExp(x);
+    function sqrt(uint256 x) public pure returns (uint256) {
+        return MirinMath.sqrt(x);
     }
 }
