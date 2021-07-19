1e2e5bfa6f75bbca4072b5c301f5d2654727a5db
Matthew Lilley <hello@matthewlilley.com>
2021-07-19 20:36:49 +0100

    chore: fixup MirinMath test

--- test/MirinMath.test.ts
+++ test/MirinMath.test.ts
@@ -5,9 +5,11 @@ const { getBigNumber } = require("./utilities");
 
 describe("MirinMath", function () {
 
+    let mirinMathContract;
+
     before(async function () {
         const MirinMathContract = await ethers.getContractFactory("MirinMathConsumerMock");
-        MirinMath = await MirinMathContract.deploy();
+        mirinMathContract = await MirinMathContract.deploy();
     });
 
     // Input values
@@ -21,26 +23,26 @@ describe("MirinMath", function () {
                             '0xffffffffffffffff', '0xffffffffffffffffffffffffffffffff'];
 
     it("MirinMath.sqrt() returns correct values", async function () {
-        expect((await MirinMath.sqrt(0)).eq(0));
+        expect((await mirinMathContract.sqrt(0)).eq(0));
         for (var i = 0; i < 8; i += 1) {
             var testInput = BigNumber.from(arr_input[i]);
             var expectedValue = BigNumber.from(arr_out[i]);
             var expectedValueMinus1 = BigNumber.from(arr_out_minus1[i]);
 
             // 2^(2^i) - 1
-            var calculatedValueMinus1 = await MirinMath.sqrt(testInput.add(-1));
+            var calculatedValueMinus1 = await mirinMathContract.sqrt(testInput.add(-1));
             await expect(calculatedValueMinus1).eq(expectedValueMinus1);
 
             // 2 ^(2^i)
-            var calculatedValue = await MirinMath.sqrt(testInput);
+            var calculatedValue = await mirinMathContract.sqrt(testInput);
 
             await expect(calculatedValue).eq(expectedValue);
             // 2 ^(2^i) + 1
-            var calculatedValuePlus1 = await MirinMath.sqrt(testInput.add(1));
+            var calculatedValuePlus1 = await mirinMathContract.sqrt(testInput.add(1));
             await expect(calculatedValuePlus1).eq(expectedValue);
         }
         var maxTestInput = BigNumber.from('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff');
-        var calculatedValue = await MirinMath.sqrt(maxTestInput)
+        var calculatedValue = await mirinMathContract.sqrt(maxTestInput)
         expect(calculatedValue).eq(arr_out_minus1[8]);
     });
   });
