d024139431aba6a21b29b906afa55602a51f8359
Nate Tribble <nate@certora.com>
2021-07-19 11:07:43 -0700

    swapRouterTokenBalanceShouldBeZero and swapRouterBalanceShouldBeZero failing

--- spec/scripts/verifySwapRouter.sh
+++ spec/scripts/verifySwapRouter.sh
@@ -1,7 +1,9 @@
-certoraRun spec/harness/SwapRouterHarness.sol \
+certoraRun spec/harness/SwapRouterHarness.sol spec/harness/SimpleBentoBox.sol spec/harness/DummyERC20A.sol spec/harness/SymbolicPool.sol \
 	--verify SwapRouterHarness:spec/SwapRouter.spec \
-	--optimistic_loop --loop_iter 2 \
-	#--packages @openzeppelin=/Users/vasu/Documents/Certora/trident/node_modules/@openzeppelin \
-    --packages @openzeppelin=/Users/nate/Documents/Projects/Sushi/trident/node_modules/@openzeppelin \
-	--solc solc8.2 \
+	--optimistic_loop --loop_iter 1 \
+	--link SwapRouterHarness:bento=SimpleBentoBox \
+	--packages @openzeppelin=/Users/nate/Documents/Projects/Sushi/trident/node_modules/@openzeppelin \
+	--solc_map SwapRouterHarness=solc8.2,DummyERC20A=solc8.2,SymbolicPool=solc8.2,SimpleBentoBox=solc6.12 \
+	--cache Trident \
 	--staging --msg "Swap Router"
+	#--packages @openzeppelin=/Users/vasu/Documents/Certora/trident/node_modules/@openzeppelin \
