72c340c3517ceffd3030277940052763b0b584d4
Mudit Gupta <guptamudit@ymail.com>
2021-07-20 12:38:03 +0530

    Fixed bento share handling

--- contracts/base/SelfPermit.sol
+++ contracts/base/SelfPermit.sol
@@ -25,18 +25,6 @@ abstract contract SelfPermit is ISelfPermit {
     }
 
     /// @inheritdoc ISelfPermit
-    function selfPermitIfNecessary(
-        address token,
-        uint256 value,
-        uint256 deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external payable override {
-        if (IERC20(token).allowance(msg.sender, address(this)) < value) selfPermit(token, value, deadline, v, r, s);
-    }
-
-    /// @inheritdoc ISelfPermit
     function selfPermitAllowed(
         address token,
         uint256 nonce,
@@ -46,18 +34,5 @@ abstract contract SelfPermit is ISelfPermit {
         bytes32 s
     ) public payable override {
         IERC20PermitAllowed(token).permit(msg.sender, address(this), nonce, expiry, true, v, r, s);
-    }
-
-    /// @inheritdoc ISelfPermit
-    function selfPermitAllowedIfNecessary(
-        address token,
-        uint256 nonce,
-        uint256 expiry,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external payable override {
-        if (IERC20(token).allowance(msg.sender, address(this)) < type(uint256).max)
-            selfPermitAllowed(token, nonce, expiry, v, r, s);
     }
 }
