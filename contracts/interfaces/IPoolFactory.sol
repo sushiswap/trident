b27a5ca3372bdf063ba21dd4b8dc0d6444674f0e
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-07-20 18:37:44 -0400

    Update IPoolFactory.sol
    tweak spacing / make license GPL3

--- contracts/interfaces/IPoolFactory.sol
+++ contracts/interfaces/IPoolFactory.sol
@@ -1,7 +1,7 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 pragma solidity ^0.8.2;
 
 interface IPoolFactory {
-  function deployPool (bytes calldata _deployData) external returns (address);
+    function deployPool(bytes calldata _deployData) external returns (address);
 }
