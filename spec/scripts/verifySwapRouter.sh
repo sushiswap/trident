999e8c66154b889c76612eaefc5f90331d384c3f
Nurit Dor <nurit@certora.com>
2021-07-20 17:32:32 +0300

    symbolic pool

--- spec/scripts/verifySwapRouter.sh
+++ spec/scripts/verifySwapRouter.sh
@@ -1,8 +1,8 @@
 certoraRun spec/harness/SwapRouterHarness.sol spec/harness/SimpleBentoBox.sol spec/harness/DummyERC20A.sol spec/harness/SymbolicPool.sol \
 	--verify SwapRouterHarness:spec/SwapRouter.spec \
 	--optimistic_loop --loop_iter 1 \
-	--link SwapRouterHarness:bento=SimpleBentoBox \
-	--packages @openzeppelin=/Users/nate/Documents/Projects/Sushi/trident/node_modules/@openzeppelin \
+	--link SwapRouterHarness:bento=SimpleBentoBox SymbolicPool:bento=SimpleBentoBox \
+	--packages @openzeppelin=$PWD/node_modules/@openzeppelin \
 	--solc_map SwapRouterHarness=solc8.2,DummyERC20A=solc8.2,SymbolicPool=solc8.2,SimpleBentoBox=solc6.12 \
 	--cache Trident \
 	--staging --msg "Swap Router"
