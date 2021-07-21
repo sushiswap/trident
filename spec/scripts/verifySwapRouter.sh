850ea0591690d1f28d90c977eb9ec45a7dccb8c4
Nurit Dor <nurit@certora.com>
2021-07-21 18:50:45 +0300

    run script

--- spec/scripts/verifySwapRouter.sh
+++ spec/scripts/verifySwapRouter.sh
@@ -1,9 +1,11 @@
-certoraRun spec/harness/SwapRouterHarness.sol spec/harness/SimpleBentoBox.sol spec/harness/DummyERC20A.sol \
+certoraRun spec/harness/SwapRouterHarness.sol spec/harness/SimpleBentoBox.sol spec/harness/DummyERC20A.sol  spec/harness/SymbolicPool.sol \
 	--verify SwapRouterHarness:spec/SwapRouter.spec \
-	--optimistic_loop --loop_iter 1 \
-	--link SwapRouterHarness:bento=SimpleBentoBox \
+	--optimistic_loop --loop_iter 2 \
+	--link SwapRouterHarness:bento=SimpleBentoBox SymbolicPool:bento=SimpleBentoBox \
 	--packages @openzeppelin=$PWD/node_modules/@openzeppelin \
-	--solc_map SwapRouterHarness=solc8.2,DummyERC20A=solc8.2,SimpleBentoBox=solc6.12 \
+	--solc_map SwapRouterHarness=solc8.2,DummyERC20A=solc8.2,SimpleBentoBox=solc6.12,SymbolicPool=solc8.2 \
+	--settings -ignoreViewFunctions \
 	--cache Trident \
-	--staging --msg "Swap Router"
+	--staging --msg "Swap Router : $1"
 	#--packages @openzeppelin=/Users/vasu/Documents/Certora/trident/node_modules/@openzeppelin \
+	#--rule $1
