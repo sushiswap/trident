f122e4712f4fce256e4ad5a96bd52dd0cfa1faf1
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-07-23 08:45:33 -0400

    Update ConstantProductPool.sol
    conform error messages / minor style, spacing nits

--- contracts/pool/ConstantProductPool.sol
+++ contracts/pool/ConstantProductPool.sol
@@ -11,7 +11,7 @@ import "hardhat/console.sol";
 import "../deployer/MasterDeployer.sol";
 
 /// @dev This pool swaps between bento shares. It does not care about underlying amounts.
-contract ConstantProductPool is MirinERC20, IPool {
+contract ConstantProductPool is IPool, MirinERC20 {
     event Mint(address indexed sender, uint256 amount0, uint256 amount1, address indexed to);
     event Burn(address indexed sender, uint256 amount0, uint256 amount1, address indexed to);
 
@@ -36,7 +36,7 @@ contract ConstantProductPool is MirinERC20, IPool {
 
     uint256 private unlocked = 1;
     modifier lock() {
-        require(unlocked == 1, "MIRIN: LOCKED");
+        require(unlocked == 1, "LOCKED");
         unlocked = 2;
         _;
         unlocked = 1;
@@ -47,10 +47,10 @@ contract ConstantProductPool is MirinERC20, IPool {
     constructor(bytes memory _deployData, address _masterDeployer) MirinERC20() {
         (IERC20 tokenA, IERC20 tokenB, uint256 _swapFee) = abi.decode(_deployData, (IERC20, IERC20, uint256));
 
-        require(address(tokenA) != address(0), "MIRIN: ZERO_ADDRESS");
-        require(address(tokenB) != address(0), "MIRIN: ZERO_ADDRESS");
-        require(tokenA != tokenB, "MIRIN: IDENTICAL_ADDRESSES");
-        require(_swapFee <= MAX_FEE, "MIRIN: INVALID_SWAP_FEE");
+        require(address(tokenA) != address(0), "ZERO_ADDRESS");
+        require(address(tokenB) != address(0), "ZERO_ADDRESS");
+        require(tokenA != tokenB, "IDENTICAL_ADDRESSES");
+        require(_swapFee <= MAX_FEE, "INVALID_SWAP_FEE");
 
         (IERC20 _token0, IERC20 _token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         token0 = _token0;
@@ -80,7 +80,7 @@ contract ConstantProductPool is MirinERC20, IPool {
             uint256 k = MirinMath.sqrt(uint256(_reserve0) * _reserve1);
             liquidity = ((computed - k) * _totalSupply) / k;
         }
-        require(liquidity > 0, "MIRIN: INSUFFICIENT_LIQUIDITY_MINTED");
+        require(liquidity > 0, "INSUFFICIENT_LIQUIDITY_MINTED");
         _mint(to, liquidity);
         _update(balance0, balance1);
         kLast = computed;
@@ -145,13 +145,12 @@ contract ConstantProductPool is MirinERC20, IPool {
             amount = amount0;
         } else {
             // Swap token0 for token1
-            require(tokenOut == address(token1), "Invalid output token");
+            require(tokenOut == address(token1), "INVALID_OUTPUT_TOKEN");
             amount1 += _getAmountOut(amount0, _reserve0 - amount0, _reserve1 - amount1);
             _transfer(token1, amount1, to, unwrapBento);
             balance1 -= amount1;
             amount = amount1;
         }
-
         _update(balance0, balance1);
         kLast = MirinMath.sqrt(balance0 * balance1);
         emit Burn(msg.sender, amount0, amount1, to);
@@ -168,14 +167,14 @@ contract ConstantProductPool is MirinERC20, IPool {
         uint256 amountIn;
 
         if (tokenIn == address(token0)) {
-            require(tokenOut == address(token1), "Invalid output token");
+            require(tokenOut == address(token1), "INVALID_OUTPUT_TOKEN");
             amountIn = balance0 - _reserve0;
             amountOut = _getAmountOut(amountIn, _reserve0, _reserve1);
             _transfer(token1, amountOut, recipient, unwrapBento);
             _update(balance0, balance1 - amountOut);
         } else {
-            require(tokenIn == address(token1), "Invalid input token");
-            require(tokenOut == address(token0), "Invalid output token");
+            require(tokenIn == address(token1), "INVALID_INPUT_TOKEN");
+            require(tokenOut == address(token0), "INVALID_OUTPUT_TOKEN");
             amountIn = balance1 - _reserve1;
             amountOut = _getAmountOut(amountIn, _reserve1, _reserve0);
             _transfer(token0, amountOut, recipient, unwrapBento);
@@ -195,28 +194,27 @@ contract ConstantProductPool is MirinERC20, IPool {
         (uint128 _reserve0, uint128 _reserve1) = (reserve0, reserve1);
 
         if (tokenIn == address(token0)) {
-            require(tokenOut == address(token1), "Invalid output token");
+            require(tokenOut == address(token1), "INVALID_OUTPUT_TOKEN");
 
             amountOut = _getAmountOut(amountIn, _reserve0, _reserve1);
             _processSwap(tokenIn, tokenOut, recipient, amountIn, amountOut, context, unwrapBento);
 
             (uint256 balance0, uint256 balance1) = _balance();
-            require(balance0 - _reserve0 >= amountIn, "Insuffficient amount in");
+            require(balance0 - _reserve0 >= amountIn, "INSUFFICIENT_AMOUNT_IN");
 
             _update(balance0, balance1 - amountOut);
         } else {
-            require(tokenIn == address(token1), "Invalid input token");
-            require(tokenOut == address(token0), "Invalid output token");
+            require(tokenIn == address(token1), "INVALID_INPUT_TOKEN");
+            require(tokenOut == address(token0), "INVALID_OUTPUT_TOKEN");
 
             amountOut = _getAmountOut(amountIn, _reserve1, _reserve0);
             _processSwap(tokenIn, tokenOut, recipient, amountIn, amountOut, context, unwrapBento);
 
             (uint256 balance0, uint256 balance1) = _balance();
-            require(balance1 - _reserve1 >= amountIn, "Insuffficient amount in");
+            require(balance1 - _reserve1 >= amountIn, "INSUFFICIENT_AMOUNT_IN");
 
             _update(balance0 - amountOut, balance1);
         }
-
         emit Swap(recipient, tokenIn, tokenOut, amountIn, amountOut);
     }
 
@@ -230,11 +228,11 @@ contract ConstantProductPool is MirinERC20, IPool {
         bool unwrapBento
     ) internal {
         _transfer(IERC20(tokenOut), amountOut, to, unwrapBento);
-        if (data.length > 0) ITridentCallee(to).tridentCallback(tokenIn, tokenOut, amountIn, amountOut, data);
+        if (data.length != 0) ITridentCallee(to).tridentCallback(tokenIn, tokenOut, amountIn, amountOut, data);
     }
 
     function _update(uint256 balance0, uint256 balance1) internal {
-        require(balance0 < type(uint128).max && balance1 < type(uint128).max, "MIRIN: OVERFLOW");
+        require(balance0 < type(uint128).max && balance1 < type(uint128).max, "OVERFLOW");
         reserve0 = uint128(balance0);
         reserve1 = uint128(balance1);
     }
@@ -249,10 +247,10 @@ contract ConstantProductPool is MirinERC20, IPool {
             computed = MirinMath.sqrt(uint256(_reserve0) * _reserve1);
             if (computed > _kLast) {
                 // barFee % of increase in liquidity
-                // NB It's going to be slihgtly less than barFee % in reality due to the Math
+                // NB It's going to be slightly less than barFee % in reality due to the Math
                 uint256 barFee = MasterDeployer(masterDeployer).barFee();
                 uint256 liquidity = (_totalSupply * (computed - _kLast) * barFee) / computed / MAX_FEE;
-                if (liquidity > 0) {
+                if (liquidity != 0) {
                     _mint(barFeeTo, liquidity);
                 }
             }
@@ -309,8 +307,6 @@ contract ConstantProductPool is MirinERC20, IPool {
             // Swap tokens to be in order
             (liquidityInputs[0], liquidityInputs[1]) = (liquidityInputs[1], liquidityInputs[0]);
         }
-        uint128 _reserve0;
-        uint128 _reserve1;
         liquidityAmount[] memory liquidityOptimal = new liquidityAmount[](2);
         liquidityOptimal[0] = liquidityAmount({
             token: liquidityInputs[0].token,
@@ -321,12 +317,11 @@ contract ConstantProductPool is MirinERC20, IPool {
             amount: liquidityInputs[1].amountDesired
         });
 
-        (_reserve0, _reserve1) = (reserve0, reserve1);
+        (uint128 _reserve0, uint128 _reserve1) = (reserve0, reserve1);
 
         if (_reserve0 == 0) {
             return liquidityOptimal;
         }
-
         uint256 amount1Optimal = (liquidityInputs[0].amountDesired * _reserve1) / _reserve0;
         if (amount1Optimal <= liquidityInputs[1].amountDesired) {
             require(amount1Optimal >= liquidityInputs[1].amountMin, "INSUFFICIENT_B_AMOUNT");
@@ -336,7 +331,6 @@ contract ConstantProductPool is MirinERC20, IPool {
             require(amount0Optimal >= liquidityInputs[0].amountMin, "INSUFFICIENT_A_AMOUNT");
             liquidityOptimal[0].amount = amount0Optimal;
         }
-
         return liquidityOptimal;
     }
 }
