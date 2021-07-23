aeda1019c47676c6b5aa1a9fd41aea67f5483199
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-07-23 16:00:13 -0400

    Update ISwapRouter.sol
    nit spacing / add natspec header

--- contracts/interfaces/ISwapRouter.sol
+++ contracts/interfaces/ISwapRouter.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
 pragma solidity >=0.7.5;
 pragma abicoder v2;
 
+/// @notice Interface for Trident pool swaps.
 interface ISwapRouter {
     // TODO: Make context optional
 
