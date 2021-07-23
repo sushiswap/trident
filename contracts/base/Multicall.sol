d1809b363ce2e16079fd130c37e0476338d31fcd
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-07-23 09:38:09 -0400

    Update Multicall.sol
    remove 'abstract' as we implement all of `IMulticall`

--- contracts/base/Multicall.sol
+++ contracts/base/Multicall.sol
@@ -6,7 +6,7 @@ import "../interfaces/IMulticall.sol";
 
 /// @title Multicall
 /// @notice Enables calling multiple methods in a single call to the contract
-abstract contract Multicall is IMulticall {
+contract Multicall is IMulticall {
     /// @inheritdoc IMulticall
     function multicall(bytes[] calldata data) external payable override returns (bytes[] memory results) {
         results = new bytes[](data.length);
