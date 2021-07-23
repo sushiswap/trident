857a09007d3d4694f3d203cb468b505cd94243d8
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-07-23 15:06:24 -0400

    Update IMulticall.sol
    conform pragma header to `MultiCall`

--- contracts/interfaces/IMulticall.sol
+++ contracts/interfaces/IMulticall.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity >=0.7.5;
-pragma abicoder v2;
+pragma solidity ^0.8.2;
 
 /// @title Multicall interface
 /// @notice Enables calling multiple methods in a single call to the contract
