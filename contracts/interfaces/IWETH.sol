d74c7766e2f398a3c8a59a83fd9d108af82cc4df
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-07-23 16:05:21 -0400

    Update IWETH.sol
    add comment / nit spacing

--- contracts/interfaces/IWETH.sol
+++ contracts/interfaces/IWETH.sol
@@ -2,12 +2,10 @@
 
 pragma solidity >=0.5.0;
 
+/// @notice Interface for wrapped ether (v9) interactions
 interface IWETH {
     function deposit() external payable;
-
     function withdraw(uint256) external;
-
     function transfer(address recipient, uint256 amount) external returns (bool);
-
     function balanceOf(address account) external view returns (uint256);
 }
