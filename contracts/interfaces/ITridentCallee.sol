0265743ffb4fbcd94e284ebef7abe421c1b86331
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-07-24 19:25:54 -0400

    Update ITridentCallee.sol
    nit compiler

--- contracts/interfaces/ITridentCallee.sol
+++ contracts/interfaces/ITridentCallee.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pragma solidity ^0.8.2;
+pragma solidity >=0.8.0;
 
-/// @notice Interface for Trident pool interactions with data context
+/// @notice Interface for Trident pool interactions with data context.
 interface ITridentCallee {
     function tridentCallback(
         address tokenIn,
