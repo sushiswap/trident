b4d41b02889d160ea62ea8f7541f225a6f37dbde
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-07-25 17:59:12 -0400

    Update and rename MirinMath.test.js to Sqrt.test.js
    conform to pool test (ConstantProduct)

--- test/Sqrt.test.js
+++ test/Sqrt.test.js
@@ -3,11 +3,11 @@ const { ethers } = require("hardhat");
 const { BigNumber } = require("ethers");
 const { getBigNumber } = require("./utilities");
 
-describe("MirinMath", function () {
+describe("Sqrt", function () {
 
     before(async function () {
-        const MirinMathContract = await ethers.getContractFactory("MirinMathConsumerMock");
-        MirinMath = await MirinMathContract.deploy();
+        const SqrtContract = await ethers.getContractFactory("SqrtMock");
+        Sqrt = await SqrtContract.deploy();
     });
 
     // Input values
@@ -20,27 +20,27 @@ describe("MirinMath", function () {
     var arr_out_minus1 = ['0x1', '0x1', '0x3', '0xf', '0xff', '0xffff', '0xffffffff',
                             '0xffffffffffffffff', '0xffffffffffffffffffffffffffffffff'];
 
-    it("MirinMath.sqrt() returns correct values", async function () {
-        expect((await MirinMath.sqrt(0)).eq(0));
+    it("Sqrt.sqrt() returns correct values", async function () {
+        expect((await Sqrt.sqrt(0)).eq(0));
         for (var i = 0; i < 8; i += 1) {
             var testInput = BigNumber.from(arr_input[i]);
             var expectedValue = BigNumber.from(arr_out[i]);
             var expectedValueMinus1 = BigNumber.from(arr_out_minus1[i]);
 
             // 2^(2^i) - 1
-            var calculatedValueMinus1 = await MirinMath.sqrt(testInput.add(-1));
+            var calculatedValueMinus1 = await Sqrt.sqrt(testInput.add(-1));
             await expect(calculatedValueMinus1).eq(expectedValueMinus1);
 
             // 2 ^(2^i)
-            var calculatedValue = await MirinMath.sqrt(testInput);
+            var calculatedValue = await Sqrt.sqrt(testInput);
 
             await expect(calculatedValue).eq(expectedValue);
             // 2 ^(2^i) + 1
-            var calculatedValuePlus1 = await MirinMath.sqrt(testInput.add(1));
+            var calculatedValuePlus1 = await Sqrt.sqrt(testInput.add(1));
             await expect(calculatedValuePlus1).eq(expectedValue);
         }
         var maxTestInput = BigNumber.from('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff');
-        var calculatedValue = await MirinMath.sqrt(maxTestInput)
+        var calculatedValue = await Sqrt.sqrt(maxTestInput)
         expect(calculatedValue).eq(arr_out_minus1[8]);
     });
   });
