dd4eeeb3b8bd56e7fea24643d957b5a6ef1749c3
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-07-27 11:30:36 -0400

    Update TridentNFT.sol
    cleanups

--- contracts/pool/TridentNFT.sol
+++ contracts/pool/TridentNFT.sol
@@ -2,13 +2,14 @@
 
 pragma solidity >=0.8.0;
 
+/// @notice Contract for Trident pool ERC-721.
 contract TridentNFT {
-    uint256 public totalSupply; // @dev tracks total unique liquidity range positions.
+    uint256 public totalSupply; // @dev Tracks total unique liquidity range positions.
     string constant public name = "TridentNFT";
     string constant public symbol = "tNFT";
     string constant public baseURI = "";
     
-    mapping(address => uint256) public balanceOf; // @dev tracks liquidity range positions held by an account.
+    mapping(address => uint256) public balanceOf; // @dev Tracks liquidity range positions held by an account.
     mapping(uint256 => address) public getApproved;
     mapping(uint256 => address) public ownerOf;
     mapping(uint256 => string) public tokenURI;
@@ -18,8 +19,8 @@ contract TridentNFT {
     event ApprovalForAll(address indexed approver, address indexed operator, bool approved);
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
     
-    mapping(int24 => mapping(int24 => TickPool)) public tickPools;
-    struct TickPool { // @dev virtual pool for concentrated liquidity in tick range.
+    mapping(int24 => mapping(int24 => TickRange)) public tickRanges;
+    struct TickRange { // @dev Virtual pool for concentrated liquidity in tick range.
         uint128 reserve0; // to-do consolidate to liquidity?
         uint128 reserve1; // to-do consolidate to liquidity?
         uint112 liquidity; // last range liquidity 
@@ -27,7 +28,7 @@ contract TridentNFT {
         mapping(address => uint256) balanceOf; // account provider range mint balance
     }
     
-    mapping(uint256 => Range) public ranges; // @dev tracks range by tokenId.
+    mapping(uint256 => Range) public ranges; // @dev Tracks range by tokenId.
     struct Range { 
         int24 lower; 
         int24 upper; 
@@ -39,7 +40,7 @@ contract TridentNFT {
     }
 
     function supportsInterface(bytes4 sig) external pure returns (bool) {
-        return (sig == 0x80ac58cd || sig == 0x5b5e139f); // ERC-165
+        return (sig == 0x80ac58cd || sig == 0x5b5e139f); // @dev ERC-165.
     }
     
     function approve(address spender, uint256 tokenId) external {
@@ -60,16 +61,16 @@ contract TridentNFT {
         address recipient, 
         uint256 amount
     ) internal {
-        tickPools[lower][upper].totalSupply += amount;
+        tickRanges[lower][upper].totalSupply += amount;
         unchecked {
-            tickPools[lower][upper].balanceOf[recipient] += amount;
+            tickRanges[lower][upper].balanceOf[recipient] += amount;
         }
-        if (tickPools[lower][upper].balanceOf[recipient] == 0) {
+        if (tickRanges[lower][upper].balanceOf[recipient] == 0) {
             totalSupply++;
             uint256 tokenId = totalSupply;
             ranges[tokenId].lower = lower;
             ranges[tokenId].upper = upper;
-            emit Transfer(address(0), recipient, tokenId); // @dev notices opening position by minting NFT.
+            emit Transfer(address(0), recipient, tokenId); // @dev Notices opening position by minting NFT.
         }
     }
 
@@ -80,13 +81,13 @@ contract TridentNFT {
     ) internal {
         int24 lower = ranges[tokenId].lower;
         int24 upper = ranges[tokenId].upper;
-        tickPools[lower][upper].balanceOf[from] -= amount;
+        tickRanges[lower][upper].balanceOf[from] -= amount;
         unchecked {
-            tickPools[lower][upper].totalSupply -= amount;
+            tickRanges[lower][upper].totalSupply -= amount;
         }
-        if (tickPools[lower][upper].balanceOf[from] == 0) {
+        if (tickRanges[lower][upper].balanceOf[from] == 0) {
             totalSupply--;
-            emit Transfer(from, address(0), tokenId); // @dev notices closing position by burning NFT.
+            emit Transfer(from, address(0), tokenId); // @dev Notices closing position by burning NFT.
         }
     }
 
@@ -97,8 +98,8 @@ contract TridentNFT {
         getApproved[tokenId] = address(0);
         ownerOf[tokenId] = recipient;
         (int24 lower, int24 upper) = getRangeById(tokenId);
-        tickPools[lower][upper].balanceOf[recipient] = tickPools[lower][upper].balanceOf[msg.sender]; // @dev update recipient balance.
-        tickPools[lower][upper].balanceOf[msg.sender] = 0; // @dev nullify sender balance.
+        tickRanges[lower][upper].balanceOf[recipient] = tickRanges[lower][upper].balanceOf[msg.sender]; // @dev Update recipient balance.
+        tickRanges[lower][upper].balanceOf[msg.sender] = 0; // @dev Nullify sender balance.
         emit Transfer(msg.sender, recipient, tokenId); 
     }
     
@@ -110,8 +111,8 @@ contract TridentNFT {
         getApproved[tokenId] = address(0);
         ownerOf[tokenId] = recipient;
         (int24 lower, int24 upper) = getRangeById(tokenId);
-        tickPools[lower][upper].balanceOf[recipient] = tickPools[lower][upper].balanceOf[owner]; // @dev update recipient balance.
-        tickPools[lower][upper].balanceOf[owner] = 0; // @dev nullify sender balance.
+        tickRanges[lower][upper].balanceOf[recipient] = tickRanges[lower][upper].balanceOf[owner]; // @dev Update recipient balance.
+        tickRanges[lower][upper].balanceOf[owner] = 0; // @dev Nullify sender balance.
         emit Transfer(owner, recipient, tokenId); 
     }
 }
