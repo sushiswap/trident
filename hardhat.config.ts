07ccce7bbff37ae82a6b3420f2529ce46aa0a355
Matthew Lilley <hello@matthewlilley.com>
2021-07-28 06:33:19 +0100

    chore: match solhint solc version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -277,7 +277,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.6",
+        version: "0.8.2",
         settings: {
           optimizer: {
             enabled: true,
