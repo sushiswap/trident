ad3b649c79c92363e394319fe7ad7049a16d3227
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-08-13 09:15:46 -0400

    Update DyDxMath.sol
    formatting

--- contracts/libraries/concentratedPool/DyDxMath.sol
+++ contracts/libraries/concentratedPool/DyDxMath.sol
@@ -1,54 +1,41 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.2;
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.8.0;
 
 import "./FullMath.sol";
 import "./UnsafeMath.sol";
 
 library DyDxMath {
-
-  function getDx(
-    uint256 liquidity,
-    uint256 priceLower,
-    uint256 priceUpper,
-    bool roundUp
-  ) internal pure returns(uint256) {
-
-    if (roundUp) {
-      
-      return UnsafeMath.divRoundingUp(
-        FullMath.mulDivRoundingUp(
-          liquidity << 96,
-          priceUpper - priceLower,
-          priceUpper
-        ),
-        priceLower
-      );
-
-    } else {
-      
-      return FullMath.mulDiv(liquidity << 96, priceUpper - priceLower, priceUpper) / priceLower;
-    
-    }
-                
-  }
-
-  function getDy(
-    uint256 liquidity,
-    uint256 priceLower,
-    uint256 priceUpper,
-    bool roundUp
-  ) internal pure returns(uint256) {
-    
-    if (roundUp) {
-      
-      return FullMath.mulDivRoundingUp(liquidity, priceUpper - priceLower, 0x1000000000000000000000000);
-    
-    } else {
-
-      return FullMath.mulDiv(liquidity, priceUpper - priceLower, 0x1000000000000000000000000);
-    
+    function getDx(
+        uint256 liquidity,
+        uint256 priceLower,
+        uint256 priceUpper,
+        bool roundUp
+    ) internal pure returns(uint256) {
+        if (roundUp) {
+        return UnsafeMath.divRoundingUp(
+            FullMath.mulDivRoundingUp(
+                liquidity << 96,
+                priceUpper - priceLower,
+                priceUpper
+            ),
+            priceLower
+        );
+        } else {
+            return FullMath.mulDiv(liquidity << 96, priceUpper - priceLower, priceUpper) / priceLower;
+        }
     }
 
-  }
-
+    function getDy(
+        uint256 liquidity,
+        uint256 priceLower,
+        uint256 priceUpper,
+        bool roundUp
+    ) internal pure returns(uint256) {
+        if (roundUp) {
+            return FullMath.mulDivRoundingUp(liquidity, priceUpper - priceLower, 0x1000000000000000000000000);
+        } else {
+            return FullMath.mulDiv(liquidity, priceUpper - priceLower, 0x1000000000000000000000000);
+        }
+    }
 }
