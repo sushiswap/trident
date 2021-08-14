3fd0ba1ef6478a6d3957d83dde637dc0cd3b4dfb
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-08-14 17:04:21 -0400

    Update TickMath.sol
    minor formatting change for trident

--- contracts/libraries/concentratedPool/TickMath.sol
+++ contracts/libraries/concentratedPool/TickMath.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.8.2;
 
-// copied from https://github.com/Uniswap/uniswap-v3-core/blob/main/contracts/libraries/TickMath.sol
+pragma solidity >=0.8.0;
 
-/// @title Math library for computing sqrt prices from ticks and vice versa
+/// @title Math library for computing sqrt prices from ticks and vice versa.
 /// @notice Computes sqrt price for ticks of size 1.0001, i.e. sqrt(1.0001^tick) as fixed point Q64.96 numbers. Supports
-/// prices between 2**-128 and 2**128
+/// prices between 2**-128 and 2**128.
+/// @author Adapted from https://github.com/Uniswap/uniswap-v3-core/blob/main/contracts/libraries/TickMath.sol. 
 library TickMath {
     /// @dev The minimum tick that may be passed to #getSqrtRatioAtTick computed from log base 1.0001 of 2**-128
     int24 internal constant MIN_TICK = -887272;
