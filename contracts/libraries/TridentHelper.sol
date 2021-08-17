2dd9966056780ecee6a046a43f227aa72217ca2d
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-08-16 22:24:30 -0400

    Update TridentHelper.sol
    comment cleanups / conform variables

--- contracts/libraries/TridentHelper.sol
+++ contracts/libraries/TridentHelper.sol
@@ -2,23 +2,21 @@
 
 pragma solidity >=0.8.0;
 
-/// @notice Library for optimized Trident exchange interactions - adapted from boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol, License-Identifier: MIT.
+/// @notice Trident router helper library.
 library TridentHelper {
     /// @notice Provides gas-optimized balance check on this contract to avoid redundant extcodesize check in addition to returndatasize check.
-    /// @dev Reverts on failed {balanceOf}.
     /// @param token Address of ERC-20 token.
-    /// @return amount Token amount.
-    function balanceOfThis(address token) internal view returns (uint256 amount) {
+    /// @return balance Token amount held by this contract.
+    function balanceOfThis(address token) internal view returns (uint256 balance) {
         (bool success, bytes memory data) = token.staticcall(abi.encodeWithSelector(0x70a08231, address(this))); // @dev balanceOf(address).
         require(success && data.length >= 32, "BALANCE_OF_FAILED");
-        amount = abi.decode(data, (uint256));
+        balance = abi.decode(data, (uint256));
     }
 
-    /// @notice Provides safe ERC20.transfer for tokens that do not consistently return true/false.
-    /// @dev Reverts on failed {transfer}.
+    /// @notice Provides 'safe' ERC-20 {transfer} for tokens that don't consistently return true/false.
     /// @param token Address of ERC-20 token.
     /// @param recipient Account to send tokens to.
-    /// @param amount The token amount to send.
+    /// @param amount Token amount to send.
     function safeTransfer(
         address token,
         address recipient,
@@ -28,8 +26,7 @@ library TridentHelper {
         require(success && (data.length == 0 || abi.decode(data, (bool))), "TRANSFER_FAILED");
     }
 
-    /// @notice Provides safe ERC20.transferFrom for tokens that do not consistently return true/false.
-    /// @dev Reverts on failed {transferFrom}.
+    /// @notice Provides 'safe' ERC-20 {transferFrom} for tokens that don't consistently return true/false.
     /// @param token Address of ERC-20 token.
     /// @param from Account to send tokens from.
     /// @param recipient Account to send tokens to.
@@ -44,16 +41,15 @@ library TridentHelper {
         require(success && (data.length == 0 || abi.decode(data, (bool))), "TRANSFER_FROM_FAILED");
     }
 
-    /// @notice Provides gas-optimized {balanceOfThis} check on `wETH` and matching {withdraw}.
-    /// @dev Reverts on failed {withdraw}.
+    /// @notice Provides low-level `wETH` {withdraw}.
     /// @param wETH Address of ERC-20 token for wrapped ETH.
-    function withdrawFromWETH(address wETH, uint256 balanceWETH) internal {
-        (bool success, ) = wETH.call(abi.encodeWithSelector(0x2e1a7d4d, balanceWETH)); // @dev withdraw(uint256).
+    /// @param amount Token amount to unwrap into ETH.
+    function withdrawFromWETH(address wETH, uint256 amount) internal {
+        (bool success, ) = wETH.call(abi.encodeWithSelector(0x2e1a7d4d, amount)); // @dev withdraw(uint256).
         require(success, "WITHDRAW_FROM_WETH_FAILED");
     }
 
-    /// @notice Provides safe ETH transfer.
-    /// @dev Reverts on failed {call}.
+    /// @notice Provides 'safe' ETH transfer.
     /// @param recipient Account to send ETH.
     /// @param amount ETH amount to send.
     function safeTransferETH(address recipient, uint256 amount) internal {
