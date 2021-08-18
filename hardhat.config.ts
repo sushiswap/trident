490f67ad8e8c8eac5c7d12efb295df5bd939ce87
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-08-18 16:06:15 -0400

    Update hardhat.config.ts
    bump solidity upper to `0.8.7`

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -268,7 +268,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.6",
+        version: "0.8.7",
         settings: {
           optimizer: {
             enabled: true,
