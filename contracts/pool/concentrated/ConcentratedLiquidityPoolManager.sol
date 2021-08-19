912bb629f28a0aa56e36f8bf170b919ea861c846
unknown <gasper.brvar@gmail.com>
2021-08-19 20:33:26 +0200

    [wip] manager contract

--- contracts/pool/concentrated/ConcentratedLiquidityPoolManager.sol
+++ contracts/pool/concentrated/ConcentratedLiquidityPoolManager.sol
@@ -4,21 +4,106 @@ pragma solidity >=0.8.0;
 
 import "../../interfaces/IPool.sol";
 
+interface IConcentratedLiquidityPool is IPool {
+    function feeGrowthGlobal0() external view returns (uint256);
+
+    function rangeFeeGrowth(int24 lowerTick, int24 upperTick) external view returns (uint256, uint256);
+}
+
 /// @dev combines the nonfungible position manager and the staking contract in one
 contract ConcentratedLiquidityPoolManager {
     struct Position {
+        IConcentratedLiquidityPool pool;
         int24 lower;
         int24 upper;
         uint128 liquidity;
         address owner;
-        uint256 feeGrowthInside0Last;
+        uint32 createdAt;
     }
 
     uint256 public positionCount;
 
-    mapping(uint256 => Position) public positions;
+    mapping(uint256 => Position) public positions; // nft id
 
-    function mint(IPool pool, bytes memory mintData) public {
+    struct Incentive {
+        uint256 feeGrowthStart; /// 1m usdc
+        uint256 feeGrowthEnd; /// 2m usdc
+        uint256 totalFeeGrowthClaimed; /// 0 to current growth
+        uint256 totalRewardUnclaimed; /// reward 100k sushi
+        uint32 expiry;
+        address owner;
+    }
+
+    struct Stake {
+        uint256 feeGrowthLast;
+        uint256 liquidity;
+        uint32 createdAt;
+    }
+
+    mapping(IConcentratedLiquidityPool => uint256) public incentiveCount;
+
+    mapping(IConcentratedLiquidityPool => mapping(uint256 => Incentive)) public incentives;
+
+    mapping(uint256 => mapping(uint256 => Stake)) public stakes; // stakes[positionId][incentiveId]
+
+    function addIncentive(IConcentratedLiquidityPool pool, Incentive memory incentive) public {
+        uint256 current = pool.feeGrowthGlobal0();
+        require(current <= incentive.feeGrowthStart, "");
+        require(current <= incentive.feeGrowthEnd, "");
+        require(incentive.feeGrowthStart < incentive.feeGrowthEnd, "");
+        require(incentive.totalRewardUnclaimed > 0, "");
+        incentives[pool][incentiveCount[pool]++] = incentive;
+        // transfer incentive in
+    }
+
+    function reclaimIncentive(
+        IConcentratedLiquidityPool pool,
+        uint256 incentiveId,
+        uint256 amount,
+        address receiver
+    ) public {
+        Incentive storage incentive = incentives[pool][incentiveId];
+        require(incentive.owner == msg.sender, "");
+        require(incentive.expiry < block.number, "");
+        require(incentive.totalRewardUnclaimed >= amount, "");
+        // transfer incentive out
+    }
+
+    // "subscribe" to the incentive
+    function subscribe(uint256 positionId, uint256 incentiveId) public {
+        Position storage position = positions[positionId];
+        require(incentiveId <= incentiveCount[position.pool], "");
+        require(position.owner == msg.sender, "");
+        require(position.createdAt < block.number && position.upper - position.lower > 100, ""); // 🤔 get rid of this probs
+        (uint256 feeGrowth0, ) = position.pool.rangeFeeGrowth(position.lower, position.upper);
+        stakes[positionId][incentiveId] = Stake(feeGrowth0, position.liquidity, uint32(block.number));
+    }
+
+    function claimReward(uint256 incentiveId, uint256 positionId) public {
+        Position storage position = positions[positionId];
+        IConcentratedLiquidityPool pool = position.pool;
+        Incentive storage incentive = incentives[pool][positionId];
+        Stake storage stake = stakes[positionId][incentiveId];
+        uint256 currentFeeGrowth = pool.feeGrowthGlobal0();
+        if (currentFeeGrowth < incentive.feeGrowthStart || position.owner != msg.sender) return; // after incentive anyone should be able to claim
+
+        uint256 maxFeeGrowth = currentFeeGrowth > incentive.feeGrowthEnd ? currentFeeGrowth : incentive.feeGrowthEnd; // import and use Math.max library
+
+        uint256 feeGrowthUnclaimed = maxFeeGrowth - incentive.totalFeeGrowthClaimed;
+
+        (uint256 feeGrowthLatest, ) = pool.rangeFeeGrowth(position.lower, position.upper);
+
+        uint256 rewards = (incentive.totalRewardUnclaimed * (feeGrowthLatest - stake.feeGrowthLast)) /
+            feeGrowthUnclaimed;
+
+        incentive.totalRewardUnclaimed -= rewards;
+
+        incentive.totalFeeGrowthClaimed += 0;
+
+        // transfer rewards
+    }
+
+    function mint(IConcentratedLiquidityPool pool, bytes memory mintData) public {
         (, int24 lower, , int24 upper, uint128 amount, address recipient) = abi.decode(
             mintData,
             (int24, int24, int24, int24, uint128, address)
@@ -26,10 +111,14 @@ contract ConcentratedLiquidityPoolManager {
 
         pool.mint(mintData);
 
-        positions[positionCount++] = Position(lower, upper, amount, recipient, 0);
+        positions[positionCount++] = Position(pool, lower, upper, amount, recipient, uint32(block.number));
+
+        // todo mint nft here
     }
 
-    function burn(IPool pool, bytes memory data) public {}
+    function burn(IPool pool, bytes memory data) public {
+        // todo burn nft here
+    }
 
     // transfers the funds
     function mintCallback() external {}
