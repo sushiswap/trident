c14cb34a66bab7817aacda214aabd1df48f86ba8
Mudit Gupta <guptamudit@ymail.com>
2021-08-20 12:21:05 +0530

    payable

--- test/Router.test.ts
+++ test/Router.test.ts
@@ -251,7 +251,9 @@ describe("Router", function () {
           amount: BigNumber.from(10).pow(18),
         },
       ];
-      await router.addLiquidity(liquidityInput, pool.address, 1, aliceEncoded);
+      await router.addLiquidity(liquidityInput, pool.address, 1, aliceEncoded, {
+        value: BigNumber.from(10).pow(17),
+      });
 
       let finalTotalSupply = await pool.totalSupply();
       let finalPoolWethBalance = await bento.balanceOf(
