75e28390edb9f59ccb12db23f861e4276d2e7bb4
gasperbr <42702599+gasperbr@users.noreply.github.com>
2021-08-20 20:17:58 +0200

    Update IBentoBoxMinimal.sol
    Revert changes

--- contracts/interfaces/IBentoBoxMinimal.sol
+++ contracts/interfaces/IBentoBoxMinimal.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 pragma solidity >=0.8.0;
-import "../libraries/RebaseLibrary.sol";
 
 /// @notice Minimal BentoBox vault interface. 
 /// @dev `token` is aliased as `address` from `IERC20` for simplicity.
