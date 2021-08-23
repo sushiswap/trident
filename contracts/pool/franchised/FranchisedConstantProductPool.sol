c8559b5c29e84136182d6004728399445e0bdcf2
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-08-23 11:23:11 -0400

    Create FranchisedConstantProductPool.sol
    add simple example of operated merkle-root whitelist for trident pool

---
+++ contracts/pool/franchised/FranchisedConstantProductPool.sol
@@ -0,0 +1,432 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.8.0;
+
+import "../interfaces/IPool.sol";
+import "../interfaces/ITridentCallee.sol";
+import "../libraries/TridentMath.sol";
+import "./TridentERC20.sol";
+import "../workInProgress/IMigrator.sol";
+import "../deployer/MasterDeployer.sol";
+
+/// @notice Allows anyone to join SushiSwap pool whitelist if they exist in a merkle root.
+interface IMerkleWhitelister {
+    /// @dev Returns the merkle root of the merkle tree containing accounts available to join whitelist.
+    function merkleRoot() external view returns (bytes32);
+    /// @dev Returns true if the index has been marked claimed.
+    function isWhitelisted(uint256 index) external view returns (bool);
+    /// @dev Claim spot in whitelist for an account. Reverts if the inputs are invalid.
+    function joinWhitelist(uint256 index, address account, bytes32[] calldata merkleProof) external;
+    /// @dev Set the merkle root of the merkle tree containing accounts available to join whitelist. 
+    function setMerkleRoot(bytes32 merkleRoot) external;
+    /// @dev This event is triggered whenever a {joinWhitelist} call succeeds.
+    event JoinWhitelist(uint256 index, address account);
+    /// @dev This event is triggered whenever a {setMerkleRoot} call succeeds.
+    event SetMerkleRoot(bytes32 merkleRoot); 
+}
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+/// @notice Trident exchange pool template with constant product formula for swapping between an ERC-20 token pair.
+/// @dev The reserves are stored as bento shares.
+///      The curve is applied to shares as well. This pool does not care about the underlying amounts.
+contract ConstantProductPool is IPool, TridentERC20, IMerkleWhitelister {
+    event Mint(address indexed sender, uint256 amount0, uint256 amount1, address indexed to);
+    event Burn(address indexed sender, uint256 amount0, uint256 amount1, address indexed to);
+    event Sync(uint256 reserve0, uint256 reserve1);
+
+    uint256 internal constant MINIMUM_LIQUIDITY = 1000;
+
+    uint8 internal constant PRECISION = 112;
+    uint256 internal constant MAX_FEE = 10000; // @dev 100%.
+    uint256 internal constant MAX_FEE_SQUARE = 100000000;
+    uint256 public immutable swapFee;
+    uint256 internal immutable MAX_FEE_MINUS_SWAP_FEE;
+
+    address public immutable barFeeTo;
+    address public immutable bento;
+    address public immutable masterDeployer;
+    address public immutable token0;
+    address public immutable token1;
+
+    uint256 public price0CumulativeLast;
+    uint256 public price1CumulativeLast;
+    uint256 public kLast;
+
+    uint112 internal reserve0;
+    uint112 internal reserve1;
+    uint32 internal blockTimestampLast;
+
+    bytes32 public constant override poolIdentifier = "Trident:ConstantProduct";
+
+    uint256 private unlocked;
+    modifier lock() {
+        require(unlocked == 1, "LOCKED");
+        unlocked = 2;
+        _;
+        unlocked = 1;
+    }
+    
+    bytes32 public override merkleRoot;
+    address public operator;
+    /// @dev This is a packed array of booleans.
+    mapping(uint256 => uint256) private whitelistedBitMap;
+    mapping(address => bool) private whitelisted;
+    
+    modifier approved(address account) {
+        require(whitelisted[account], '!whitelisted');
+        _;
+    }
+
+    /// @dev Only set immutable variables here - state changes made here will not be used.
+    constructor(bytes memory _deployData, address _masterDeployer) {
+        (address tokenA, address tokenB, uint256 _swapFee, bool _twapSupport, bytes32 _merkleRoot, address _operator) = abi.decode(
+            _deployData,
+            (address, address, uint256, bool, bytes32, address)
+        );
+
+        require(tokenA != address(0), "ZERO_ADDRESS");
+        require(tokenA != tokenB, "IDENTICAL_ADDRESSES");
+        require(_swapFee <= MAX_FEE, "INVALID_SWAP_FEE");
+
+        (, bytes memory _bento) = _masterDeployer.staticcall(abi.encodeWithSelector(0x4da31827)); // @dev bento().
+        (, bytes memory _barFeeTo) = _masterDeployer.staticcall(abi.encodeWithSelector(0x0c0a0cd2)); // @dev barFeeTo().
+
+        token0 = tokenA;
+        token1 = tokenB;
+        swapFee = _swapFee;
+        MAX_FEE_MINUS_SWAP_FEE = MAX_FEE - _swapFee;
+        bento = abi.decode(_bento, (address));
+        barFeeTo = abi.decode(_barFeeTo, (address));
+        masterDeployer = _masterDeployer;
+        unlocked = 1;
+        if (_twapSupport) {
+            blockTimestampLast = 1;
+        }
+        merkleRoot = _merkleRoot;
+        operator = _operator;
+    }
+    
+    /// @dev
+    /// The following functions track merkle claims for whitelisting.
+    function isWhitelisted(uint256 index) public view override returns (bool) {
+        uint256 whitelistedWordIndex = index / 256;
+        uint256 whitelistedBitIndex = index % 256;
+        uint256 claimedWord = whitelistedBitMap[whitelistedWordIndex];
+        uint256 mask = (1 << whitelistedBitIndex);
+        return claimedWord & mask == mask;
+    }
+    
+    function joinWhitelist(uint256 index, address account, bytes32[] calldata merkleProof) external override {
+        require(!isWhitelisted(index), 'Permission already claimed.');
+        
+        /// @dev Verify merkle proof.
+        bytes32 node = keccak256(abi.encodePacked(index, account));
+        require(MerkleProof.verify(merkleProof, merkleRoot, node), 'Invalid proof');
+
+        /// @dev Mark registration.
+        _setWhitelisted(index);
+        whitelisted[account] = true;
+
+        emit JoinWhitelist(index, account);
+    }
+    
+    function _setWhitelisted(uint256 index) private {
+        uint256 whitelistedWordIndex = index / 256;
+        uint256 whitelistedBitIndex = index % 256;
+        whitelistedBitMap[whitelistedWordIndex] = whitelistedBitMap[whitelistedWordIndex] | (1 << whitelistedBitIndex);
+    }
+    
+    function setMerkleRoot(bytes32 _merkleRoot) external override {
+        require(msg.sender == operator, '!operator');
+        // Set the new merkle root
+        merkleRoot = _merkleRoot;
+        emit SetMerkleRoot(_merkleRoot);
+    }
+
+    function mint(bytes calldata data) public override lock returns (uint256 liquidity) {
+        address to = abi.decode(data, (address));
+        require(whitelisted[to], '!whitelisted');
+        (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) = _getReserves();
+        (uint256 balance0, uint256 balance1) = _balance();
+        uint256 _totalSupply = totalSupply;
+
+        _mintFee(_reserve0, _reserve1, _totalSupply);
+
+        uint256 amount0 = balance0 - _reserve0;
+        uint256 amount1 = balance1 - _reserve1;
+
+        uint256 computed = TridentMath.sqrt(balance0 * balance1);
+        if (_totalSupply == 0) {
+            _mint(address(0), MINIMUM_LIQUIDITY);
+            address migrator = MasterDeployer(masterDeployer).migrator();
+            if (msg.sender == migrator) {
+                liquidity = IMigrator(migrator).desiredLiquidity();
+                require(liquidity > 0 && liquidity != type(uint256).max, "BAD_DESIRED_LIQUIDITY");
+            } else {
+                require(migrator == address(0), "ONLY_MIGRATOR");
+                liquidity = computed - MINIMUM_LIQUIDITY;
+            }
+        } else {
+            uint256 k = TridentMath.sqrt(uint256(_reserve0) * _reserve1);
+            liquidity = ((computed - k) * _totalSupply) / k;
+        }
+        require(liquidity > 0, "INSUFFICIENT_LIQUIDITY_MINTED");
+        _mint(to, liquidity);
+        _update(balance0, balance1, _reserve0, _reserve1, _blockTimestampLast);
+        kLast = computed;
+        emit Mint(msg.sender, amount0, amount1, to);
+    }
+
+    function burn(bytes calldata data) public override lock returns (IPool.TokenAmount[] memory withdrawnAmounts) {
+        (address to, bool unwrapBento) = abi.decode(data, (address, bool));
+        require(whitelisted[to], '!whitelisted');
+        (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) = _getReserves();
+        (uint256 balance0, uint256 balance1) = _balance();
+        uint256 _totalSupply = totalSupply;
+        uint256 liquidity = balanceOf[address(this)];
+
+        _mintFee(_reserve0, _reserve1, _totalSupply);
+
+        uint256 amount0 = (liquidity * balance0) / _totalSupply;
+        uint256 amount1 = (liquidity * balance1) / _totalSupply;
+
+        _burn(address(this), liquidity);
+
+        _transfer(token0, amount0, to, unwrapBento);
+        _transfer(token1, amount1, to, unwrapBento);
+
+        balance0 -= amount0;
+        balance1 -= amount1;
+
+        _update(balance0, balance1, _reserve0, _reserve1, _blockTimestampLast);
+        kLast = TridentMath.sqrt(balance0 * balance1);
+
+        withdrawnAmounts = new TokenAmount[](2);
+        withdrawnAmounts[0] = TokenAmount({token: address(token0), amount: amount0});
+        withdrawnAmounts[1] = TokenAmount({token: address(token1), amount: amount1});
+
+        emit Burn(msg.sender, amount0, amount1, to);
+    }
+
+    function burnSingle(bytes calldata data) public override lock returns (uint256 amount) {
+        (address tokenOut, address to, bool unwrapBento) = abi.decode(data, (address, address, bool));
+        require(whitelisted[to], '!whitelisted');
+        (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) = _getReserves();
+        (uint256 balance0, uint256 balance1) = _balance();
+        uint256 _totalSupply = totalSupply;
+        uint256 liquidity = balanceOf[address(this)];
+
+        _mintFee(_reserve0, _reserve1, _totalSupply);
+
+        uint256 amount0 = (liquidity * balance0) / _totalSupply;
+        uint256 amount1 = (liquidity * balance1) / _totalSupply;
+
+        _burn(address(this), liquidity);
+
+        if (tokenOut == token1) {
+            // @dev Swap token0 for token1.
+            // Calculate amountOut as if the user first withdrew balanced liquidity and then swapped token0 for token1.
+            amount1 += _getAmountOut(amount0, _reserve0 - amount0, _reserve1 - amount1);
+            _transfer(token1, amount1, to, unwrapBento);
+            balance1 -= amount1;
+            amount = amount1;
+            amount0 = 0;
+        } else {
+            // @dev Swap token1 for token0.
+            require(tokenOut == token1, "INVALID_OUTPUT_TOKEN");
+            amount0 += _getAmountOut(amount1, _reserve1 - amount1, _reserve0 - amount0);
+            _transfer(token0, amount0, to, unwrapBento);
+            balance0 -= amount0;
+            amount = amount0;
+            amount1 = 0;
+        }
+
+        _update(balance0, balance1, _reserve0, _reserve1, _blockTimestampLast);
+        kLast = TridentMath.sqrt(balance0 * balance1);
+        emit Burn(msg.sender, amount0, amount1, to);
+    }
+
+    function swap(bytes calldata data) public override lock returns (uint256 amountOut) {
+        (address tokenIn, address recipient, bool unwrapBento) = abi.decode(data, (address, address, bool));
+        require(whitelisted[recipient], '!whitelisted');
+        (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) = _getReserves();
+        (uint256 balance0, uint256 balance1) = _balance();
+        uint256 amountIn;
+        address tokenOut;
+
+        if (tokenIn == token0) {
+            tokenOut = token1;
+            amountIn = balance0 - _reserve0;
+            amountOut = _getAmountOut(amountIn, _reserve0, _reserve1);
+            balance1 -= amountOut;
+        } else {
+            require(tokenIn == token1, "INVALID_INPUT_TOKEN");
+            tokenOut = token0;
+            amountIn = balance1 - reserve1;
+            amountOut = _getAmountOut(amountIn, _reserve1, _reserve0);
+            balance0 -= amountOut;
+        }
+        _transfer(tokenOut, amountOut, recipient, unwrapBento);
+        _update(balance0, balance1, _reserve0, _reserve1, _blockTimestampLast);
+        emit Swap(recipient, tokenIn, tokenOut, amountIn, amountOut);
+    }
+
+    function flashSwap(bytes calldata data) public override lock returns (uint256 amountOut) {
+        (address tokenIn, address recipient, bool unwrapBento, uint256 amountIn, bytes memory context) = abi.decode(
+            data,
+            (address, address, bool, uint256, bytes)
+        );
+        require(whitelisted[recipient], '!whitelisted');
+        (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) = _getReserves();
+
+        if (tokenIn == token0) {
+            amountOut = _getAmountOut(amountIn, _reserve0, _reserve1);
+            _transfer(token1, amountOut, recipient, unwrapBento);
+            ITridentCallee(msg.sender).tridentSwapCallback(context);
+            (uint256 balance0, uint256 balance1) = _balance();
+            require(balance0 - _reserve0 >= amountIn, "INSUFFICIENT_AMOUNT_IN");
+            _update(balance0, balance1, _reserve0, _reserve1, _blockTimestampLast);
+            emit Swap(recipient, tokenIn, token1, amountIn, amountOut);
+        } else {
+            require(tokenIn == token1, "INVALID_INPUT_TOKEN");
+            amountOut = _getAmountOut(amountIn, _reserve1, _reserve0);
+            _transfer(token0, amountOut, recipient, unwrapBento);
+            ITridentCallee(msg.sender).tridentSwapCallback(context);
+            (uint256 balance0, uint256 balance1) = _balance();
+            require(balance1 - _reserve1 >= amountIn, "INSUFFICIENT_AMOUNT_IN");
+            _update(balance0, balance1, _reserve0, _reserve1, _blockTimestampLast);
+            emit Swap(recipient, tokenIn, token0, amountIn, amountOut);
+        }
+    }
+
+    function _getReserves()
+        internal
+        view
+        returns (
+            uint112 _reserve0,
+            uint112 _reserve1,
+            uint32 _blockTimestampLast
+        )
+    {
+        _reserve0 = reserve0;
+        _reserve1 = reserve1;
+        _blockTimestampLast = blockTimestampLast;
+    }
+
+    function _balance() internal view returns (uint256 balance0, uint256 balance1) {
+        // @dev balanceOf(address,address).
+        (, bytes memory _balance0) = bento.staticcall(abi.encodeWithSelector(0xf7888aec, token0, address(this)));
+        balance0 = abi.decode(_balance0, (uint256));
+        // @dev balanceOf(address,address).
+        (, bytes memory _balance1) = bento.staticcall(abi.encodeWithSelector(0xf7888aec, token1, address(this)));
+        balance1 = abi.decode(_balance1, (uint256));
+    }
+
+    function _update(
+        uint256 balance0,
+        uint256 balance1,
+        uint112 _reserve0,
+        uint112 _reserve1,
+        uint32 _blockTimestampLast
+    ) internal {
+        require(balance0 <= type(uint112).max && balance1 <= type(uint112).max, "OVERFLOW");
+
+        if (blockTimestampLast == 0) {
+            // @dev TWAP support is disabled for gas efficiency.
+            reserve0 = uint112(balance0);
+            reserve1 = uint112(balance1);
+        } else {
+            uint32 blockTimestamp = uint32(block.timestamp % 2**32);
+            if (blockTimestamp != _blockTimestampLast && _reserve0 != 0) {
+                unchecked {
+                    uint32 timeElapsed = blockTimestamp - _blockTimestampLast;
+                    uint256 price0 = (uint256(_reserve1) << PRECISION) / _reserve0;
+                    price0CumulativeLast += price0 * timeElapsed;
+                    uint256 price1 = (uint256(_reserve0) << PRECISION) / _reserve1;
+                    price1CumulativeLast += price1 * timeElapsed;
+                }
+            }
+            reserve0 = uint112(balance0);
+            reserve1 = uint112(balance1);
+            blockTimestampLast = blockTimestamp;
+        }
+
+        emit Sync(balance0, balance1);
+    }
+
+    function _mintFee(
+        uint112 _reserve0,
+        uint112 _reserve1,
+        uint256 _totalSupply
+    ) internal returns (uint256 computed) {
+        uint256 _kLast = kLast;
+        if (_kLast != 0) {
+            computed = TridentMath.sqrt(uint256(_reserve0) * _reserve1);
+            if (computed > _kLast) {
+                // @dev 'barFee' % of increase in liquidity.
+                // It's going to be slightly less than barFee % in reality due to the math.
+                (, bytes memory _barFee) = masterDeployer.staticcall(abi.encodeWithSelector(0xc14ad802)); // @dev barFee().
+                uint256 barFee = abi.decode(_barFee, (uint256));
+                uint256 liquidity = (_totalSupply * (computed - _kLast) * barFee) / computed / MAX_FEE;
+                if (liquidity != 0) {
+                    _mint(barFeeTo, liquidity);
+                }
+            }
+        }
+    }
+
+    function _getAmountOut(
+        uint256 amountIn,
+        uint256 reserveAmountIn,
+        uint256 reserveAmountOut
+    ) internal view returns (uint256 amountOut) {
+        uint256 amountInWithFee = amountIn * MAX_FEE_MINUS_SWAP_FEE;
+        amountOut = (amountInWithFee * reserveAmountOut) / (reserveAmountIn * MAX_FEE + amountInWithFee);
+    }
+
+    function _transfer(
+        address token,
+        uint256 shares,
+        address to,
+        bool unwrapBento
+    ) internal {
+        if (unwrapBento) {
+            // @dev withdraw(address,address,address,uint256,uint256).
+            (bool success, ) = bento.call(abi.encodeWithSelector(0x97da6d30, token, address(this), to, 0, shares));
+            require(success, "WITHDRAW_FAILED");
+        } else {
+            // @dev transfer(address,address,address,uint256).
+            (bool success, ) = bento.call(abi.encodeWithSelector(0xf18d03cc, token, address(this), to, shares));
+            require(success, "TRANSFER_FAILED");
+        }
+    }
+
+    function getAssets() public view override returns (address[] memory assets) {
+        assets = new address[](2);
+        assets[0] = token0;
+        assets[1] = token1;
+    }
+
+    function getAmountOut(bytes calldata data) public view override returns (uint256 finalAmountOut) {
+        (address tokenIn, uint256 amountIn) = abi.decode(data, (address, uint256));
+        (uint112 _reserve0, uint112 _reserve1, ) = _getReserves();
+        if (tokenIn == token0) {
+            finalAmountOut = _getAmountOut(amountIn, _reserve0, _reserve1);
+        } else {
+            finalAmountOut = _getAmountOut(amountIn, _reserve1, _reserve0);
+        }
+    }
+
+    function getReserves()
+        public
+        view
+        returns (
+            uint112 _reserve0,
+            uint112 _reserve1,
+            uint32 _blockTimestampLast
+        )
+    {
+        return _getReserves();
+    }
+}
