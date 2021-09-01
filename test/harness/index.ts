ca44c0eefaba3c143e2e0285175f427f5d0b2761
Mudit Gupta <guptamudit@ymail.com>
2021-09-01 10:53:19 +0530

    wip

--- test/harness/index.ts
+++ test/harness/index.ts
@@ -1,16 +1,18 @@
 // @ts-nocheck
 
-import { BigNumber } from "ethers";
+import { BigNumber, utils } from "ethers";
 import { Multicall } from "../../typechain/Multicall";
 import { ethers } from "hardhat";
 import { expect } from "chai";
 import { getBigNumber } from "./helpers";
+import { Token } from "@sushiswap/sdk";
 
 let accounts = [];
 // First token is used as weth
 let tokens = [];
 let pools = [];
 let bento, masterDeployer, router;
+let poolTokens = new Map();
 
 export async function initialize() {
   if (accounts.length > 0) {
@@ -81,22 +83,105 @@ export async function initialize() {
   for (let i = 0; i < tokens.length; i++) {
     for (let j = i + 1; j < tokens.length; j++) {
       // Pool deploy data
-      let addresses = [tokens[i].address, tokens[j].address].sort();
-      const deployData = ethers.utils.defaultAbiCoder.encode(
+      let token0, token1;
+      if (tokens[i].address < tokens[j].address) {
+        token0 = tokens[i];
+        token1 = tokens[j];
+      } else {
+        token0 = tokens[j];
+        token1 = tokens[i];
+      }
+      const deployData = utils.defaultAbiCoder.encode(
         ["address", "address", "uint8", "bool"],
-        [addresses[0], addresses[1], 30, false]
+        [token0.address, token1.address, 30, false]
       );
-      promises.push(
-        masterDeployer
-          .deployPool(poolFactory.address, deployData)
-          .then((tx) => {
-            return tx.wait();
-          })
-          .then((tx) => {
-            return Pool.attach(tx.events[0].args[1]);
-          })
+      const salt = utils.keccak256(deployData);
+      const constructorParams = utils.defaultAbiCoder
+        .encode(["bytes", "address"], [deployData, masterDeployer.address])
+        .substring(2);
+      const initCodeHash = utils.keccak256(Pool.bytecode + constructorParams);
+      const poolAddress = utils.getCreate2Address(
+        poolFactory.address,
+        salt,
+        initCodeHash
       );
+      poolTokens.set(poolAddress, [token0, token1]);
+      pools.push(poolAddress);
+
+      promises.push(masterDeployer.deployPool(poolFactory.address, deployData));
     }
   }
-  pools = await Promise.all(promises);
+  await Promise.all(promises);
+}
+
+export async function addLiquidity(poolNumber, amount0, amount1) {
+  let pool = pools[poolNumber];
+  let [token0, token1] = poolTokens.get(pool.address);
+  let [iTS, iPB0, iPB1, iUB0, iUB1, iUNB0, iUNB1] = await getBalances(
+    pool.address,
+    accounts[0].address,
+    token0,
+    token1
+  );
+
+  let liquidityInput = [
+    {
+      token: token0.address,
+      native: false,
+      amount: BigNumber.from(10).pow(18),
+    },
+    {
+      token: sushi.address,
+      native: false,
+      amount: BigNumber.from(10).pow(18),
+    },
+  ];
+  await router.addLiquidity(liquidityInput, pool.address, 1, aliceEncoded);
+  let intermediateTotalSupply = await pool.totalSupply();
+  let intermediatePoolWethBalance = await bento.balanceOf(
+    weth.address,
+    pool.address
+  );
+  let intermediatePoolSushiBalance = await bento.balanceOf(
+    sushi.address,
+    pool.address
+  );
+
+  expect(intermediateTotalSupply).gt(initialTotalSupply);
+  expect(intermediatePoolWethBalance).eq(
+    initialPoolWethBalance.add(BigNumber.from(10).pow(18))
+  );
+  expect(intermediatePoolSushiBalance).eq(
+    initialPoolSushiBalance.add(BigNumber.from(10).pow(18))
+  );
+  expect(
+    intermediatePoolWethBalance
+      .mul(BigNumber.from(10).pow(36))
+      .div(intermediateTotalSupply)
+  ).eq(
+    initialPoolWethBalance
+      .mul(BigNumber.from(10).pow(36))
+      .div(initialTotalSupply)
+  );
+  expect(
+    intermediatePoolSushiBalance
+      .mul(BigNumber.from(10).pow(36))
+      .div(intermediateTotalSupply)
+  ).eq(
+    initialPoolSushiBalance
+      .mul(BigNumber.from(10).pow(36))
+      .div(initialTotalSupply)
+  );
+}
+
+async function getBalances(pool, user, token0, token1) {
+  return Promise.all([
+    pool.totalSupply(),
+    bento.balanceOf(token0.address, pool),
+    bento.balanceOf(token1.address, pool),
+    bento.balanceOf(token0.address, user),
+    bento.balanceOf(token1.address, user),
+    token0.balanceOf(user),
+    token1.balanceOf(user),
+  ]);
 }
