fd64896ce92f1eaf9b6e804944418cea7cadbd17
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-09-03 10:23:46 -0400

    Update Migrator.sol
    cleanups / add code comments / simplify interface imports

--- contracts/deployer/Migrator.sol
+++ contracts/deployer/Migrator.sol
@@ -2,67 +2,64 @@
 
 pragma solidity >=0.8.0;
 
-import "@sushiswap/core/contracts/uniswapv2/interfaces/IUniswapV2Pair.sol";
-import "@sushiswap/core/contracts/uniswapv2/interfaces/IUniswapV2Factory.sol";
-import "../pool/ConstantProductPoolFactory.sol";
-import "../deployer/MasterDeployer.sol";
-import "../interfaces/IPool.sol";
-import "../interfaces/IBentoBoxMinimal.sol";
+import "../interfaces/IMigrator.sol";
 import "hardhat/console.sol";
 
-interface IERC20 {
-    function totalSupply() external returns (uint256);
-}
-
+/// @notice Trident pool migrator contract for legacy SushiSwap.
 contract Migrator {
-    address public chef;
-    ConstantProductPoolFactory public factory;
-    IBentoBoxMinimal public bentoBox;
-    uint256 public desiredLiquidity = type(uint256).max;
+    IMigrator public immutable bento;
+    IMigrator public immutable constantProductPoolFactory;
+    address public immutable masterChef;
+    uint256 public desiredLiquidity;
 
     constructor(
-        address _chef,
-        IBentoBoxMinimal _bentoBox,
-        ConstantProductPoolFactory _factory
+        IMigrator _bento,
+        IMigrator _constantProductPoolFactory,
+        address _masterChef
     ) {
-        chef = _chef;
-        bentoBox = _bentoBox;
-        factory = _factory;
-    }
-
-    function migrate(IUniswapV2Pair orig) public returns (IPool) {
-        require(msg.sender == chef, "!chef");
-
-        address token0 = orig.token0();
-        address token1 = orig.token1();
-
-        bytes memory deployData = abi.encode(token0, token1, 10, false);
-
-        IPool pair = IPool(factory.configAddress(deployData));
-
-        if (address(pair) == (address(0))) {
-            pair = IPool(factory.deployPool(deployData));
-        }
-
-        require(IERC20(address(pair)).totalSupply() == 0, "pair must have no existing supply");
-
-        uint256 lp = orig.balanceOf(msg.sender);
-
-        if (lp == 0) return pair;
-
-        orig.transferFrom(msg.sender, address(orig), lp);
-
-        desiredLiquidity = lp;
-
-        (uint256 amount0, uint256 amount1) = orig.burn(address(bentoBox));
-
-        bentoBox.deposit(token0, address(bentoBox), address(pair), amount0, 0);
-        bentoBox.deposit(token1, address(bentoBox), address(pair), amount1, 0);
-
-        pair.mint(abi.encode(msg.sender));
-
+        bento = _bento;
+        constantProductPoolFactory = _constantProductPoolFactory;
+        masterChef = _masterChef;
         desiredLiquidity = type(uint256).max;
+    }
+    
+    /// @notice Migration method to replace legacy SushiSwap liquidity tokens with Trident position.
+    /// @param oldPool Legacy SushiSwap pair pool.
+    /// @return pool Returns confirmed Trident pair pool - this is more useful where migration initializes pair.
+    function migrate(IMigrator oldPool) external returns (address pool) {
+        require(msg.sender == masterChef, "NOT_CHEF");
+        // @dev Get the `oldPool` pair tokens.
+        address token0 = oldPool.token0(); 
+        address token1 = oldPool.token1(); 
+        
+        bytes memory deployData = abi.encode(token0, token1, 10, false);
+        
+        pool = constantProductPoolFactory.configAddress(deployData);
+    
+        // @dev If `newPool` is uninitialized, deploy on Trident.
+        if (pool == address(0)) pool = constantProductPoolFactory.deployPool(deployData);
 
-        return pair;
+        // @dev Check `newPool` LP has not already initialized.
+        require(IMigrator(pool).totalSupply() == 0, "PAIR_ALREADY_SUPPLIED");
+
+        // @dev Get `oldPool` LP balance from `MasterChef`.
+        uint256 lp = oldPool.balanceOf(msg.sender);
+        desiredLiquidity = lp;
+        
+        if (lp == 0) return pool;
+        
+        // @dev Pull `oldPool` LP balance from `MasterChef` to `oldPool` for burn.
+        oldPool.transferFrom(msg.sender, address(oldPool), lp);
+
+        // @dev Complete LP migration with burn from `oldPool` and mint into `newPool` pair.
+        (uint256 amount0, uint256 amount1) = oldPool.burn(address(bento)); 
+        
+        bento.deposit(token0, address(bento), pool, amount0, 0);
+        bento.deposit(token1, address(bento), pool, amount1, 0);
+        
+        IMigrator(pool).mint(abi.encode(msg.sender));
+        
+        desiredLiquidity = type(uint256).max;
+        return pool;
     }
 }
