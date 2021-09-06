83443221a59d4a860a62f9544e8a6bc7df563efb
decanus <7621705+decanus@users.noreply.github.com>
2021-09-06 16:55:29 +0200

    fix

--- contracts/pool/IncentivizedPool.sol
+++ contracts/pool/IncentivizedPool.sol
@@ -3,13 +3,12 @@ pragma solidity ^0.8.4;
 import "./IndexPool.sol";
 
 interface ERC20 {
-    function transfer(address, uint256);
+    function transfer(address, uint256) external;
 }
 
 /// @notice A pool that simply is an incentivized version of the index pool.
 contract IncentivizedPool is IndexPool {
-
-    mapping (address => uint256) public rewardDebt;
+    mapping(address => uint256) public rewardDebt;
 
     uint256 public rewardPerShare;
     uint256 public rewardPerBlock;
@@ -26,7 +25,11 @@ contract IncentivizedPool is IndexPool {
 
     ERC20 public rewardToken;
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
         if (block.number <= lastRewardBlock) {
             return;
         }
@@ -36,32 +39,26 @@ contract IncentivizedPool is IndexPool {
         }
 
         uint256 multiplier = getMultiplier(lastRewardBlock, block.number);
-        uint256 reward = multiplier.mul(rewardPerBlock).mul(allocPoint).div(totalAllocPoint);
+        uint256 reward = (multiplier * rewardPerBlock * allocPoint) / totalAllocPoint;
 
-        accPerShare = accPerShare.add(reward.mul(1e12).div(totalSupply));
+        accPerShare = accPerShare + ((reward * 1e12) / totalSupply);
 
         lastRewardBlock = block.number;
 
-        if (balances[from] > 0) {
-            rewardToken.transfer(from, balances[from].mul(accPerShare).div(1e12).sub(rewardDebt));
+        if (balanceOf[from] > 0) {
+            rewardToken.transfer(from, ((balanceOf[from] * accPerShare) / 1e12) - rewardDebt[from]);
         }
+
+        rewardDebt[from] = ((balanceOf[from] * accPerShare) / 1e12);
     }
 
-    function getMultiplier(uint256 _from, uint256 _to)
-    public
-    view
-    returns (uint256)
-    {
+    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
         if (_to <= bonusEndBlock) {
-            return _to.sub(_from).mul(BONUS_MULTIPLIER);
+            return (_to - _from) * BONUS_MULTIPLIER;
         } else if (_from >= bonusEndBlock) {
-            return _to.sub(_from);
+            return _to - _from;
         } else {
-            return
-            bonusEndBlock.sub(_from).mul(BONUS_MULTIPLIER).add(
-                _to.sub(bonusEndBlock)
-            );
+            return (bonusEndBlock - _from) * BONUS_MULTIPLIER + (_to - (bonusEndBlock));
         }
     }
-
 }
