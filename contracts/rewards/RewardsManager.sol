2dd57d814225a0996c9a6d88aef7f91d719bdfa0
decanus <7621705+decanus@users.noreply.github.com>
2021-09-07 18:02:12 +0200

    added from masterchef

--- contracts/rewards/RewardsManager.sol
+++ contracts/rewards/RewardsManager.sol
@@ -5,13 +5,14 @@ pragma solidity >=0.8.0;
 import "../interfaces/IPool.sol";
 import "../interfaces/IRewarder.sol";
 import "../interfaces/IMasterChef.sol";
+import "../utils/TridentOwnable.sol";
 
 interface Sushi {
     function safeTransfer(address, uint256) external returns (bool);
 }
 
 /// @notice Manages the rewards for various pools without requiring users to stake LP tokens.
-contract RewardsManager {
+contract RewardsManager is TridentOwnable {
     /// @notice Info of each pool.
     /// `allocPoint` The amount of allocation points assigned to the pool.
     /// Also known as the amount of SUSHI to distribute per block.
@@ -42,10 +43,39 @@ contract RewardsManager {
 
     event Harvest(address indexed user, address indexed pid, uint256 amount);
     event LogUpdatePool(address indexed pid, uint64 lastRewardBlock, uint256 lpSupply, uint256 accSushiPerShare);
+    event LogSetPool(address indexed pid, uint256 allocPoint, IRewarder indexed rewarder, bool overwrite);
 
     constructor(IMasterChef _MASTER_CHEF, Sushi _SUSHI) {
         MASTER_CHEF = _MASTER_CHEF;
         SUSHI = _SUSHI;
+    }
+
+    /// @notice Update the given pool's SUSHI allocation point and `IRewarder` contract. Can only be called by the owner.
+    /// @param _pool The address of the pool. See `poolInfo`.
+    /// @param _allocPoint New AP of the pool.
+    /// @param _rewarder Address of the rewarder delegate.
+    /// @param overwrite True if _rewarder should be `set`. Otherwise `_rewarder` is ignored.
+    function set(
+        address _pool,
+        uint256 _allocPoint,
+        IRewarder _rewarder,
+        bool overwrite
+    ) public onlyOwner {
+        totalAllocPoint = (totalAllocPoint - poolInfo[_pool].allocPoint) + _allocPoint;
+        poolInfo[_pool].allocPoint = uint64(_allocPoint);
+        if (overwrite) {
+            rewarder[_pool] = _rewarder;
+        }
+        emit LogSetPool(_pool, _allocPoint, overwrite ? _rewarder : rewarder[_pool], overwrite);
+    }
+
+    /// @notice Update reward variables for all pools. Be careful of gas spending!
+    /// @param pools Pool addresses of all to be updated. Make sure to update all active pools.
+    function massUpdatePools(IPool[] calldata pools) external {
+        uint256 len = pools.length;
+        for (uint256 i = 0; i < len; ++i) {
+            updatePool(pools[i]);
+        }
     }
 
     function claimRewardsFor(IPool pool, address account) external {
