2fc1d7d1e2dc99c816a513b590b37cfcac594398
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-09-10 11:28:28 -0400

    Update WhiteListManager.sol
    refactoring to conform to Trident style / use `block.chainid` to reference chainId

--- contracts/pool/franchised/WhiteListManager.sol
+++ contracts/pool/franchised/WhiteListManager.sol
@@ -1,105 +1,90 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.0;
+
+pragma solidity >=0.8.0;
 
 contract WhiteListManager {
-    event LogWhiteListUser(address indexed operator, address indexed user, bool approved);
-    event LogSetMerkleRoot(address operator, bytes32 merkleRoot);
-    event LogJoinWithMerkle(address operator, uint256 indexed index, address indexed account);
-
-    mapping(address => mapping(address => bool)) public whitelistedUsers;
-
-    // merkle root variables
-
-    mapping(address => bytes32) public merkleRoot;
-    /// @dev This is a packed array of booleans.
-    mapping(address => mapping(uint256 => uint256)) internal whitelistedBitMap;
-
-    // EIP712 related variables and functions
-
-    bytes32 private constant DOMAIN_SEPARATOR_SIGNATURE_HASH = keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
-    // See https://eips.ethereum.org/EIPS/eip-191
+    event WhiteListAccount(address indexed operator, address indexed account, bool approved);
+    event SetMerkleRoot(address operator, bytes32 merkleRoot);
+    event JoinWithMerkle(address operator, uint256 indexed index, address indexed account);
+    
+    /// @notice EIP-712 related variables and functions.
     string private constant EIP191_PREFIX_FOR_EIP712_STRUCTURED_DATA = "\x19\x01";
-    bytes32 private constant APPROVAL_SIGNATURE_HASH = keccak256("SetWhitelisting(address user,bool approved,uint256 deadline)");
-
-    // solhint-disable-next-line var-name-mixedcase
+    bytes32 private constant APPROVAL_SIGNATURE_HASH = keccak256("SetWhitelisting(address account,bool approved,uint256 deadline)");
+    bytes32 private constant DOMAIN_SEPARATOR_SIGNATURE_HASH = keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
     bytes32 private immutable _DOMAIN_SEPARATOR;
-    // solhint-disable-next-line var-name-mixedcase
     uint256 private immutable DOMAIN_SEPARATOR_CHAIN_ID;
 
-    function _calculateDomainSeparator(uint256 chainId) private view returns (bytes32) {
-        return keccak256(abi.encode(DOMAIN_SEPARATOR_SIGNATURE_HASH, keccak256("WhiteListManager"), chainId, address(this)));
+    mapping(address => mapping(address => bool)) public whitelistedAccounts;
+
+    /// @notice Merkle root variables.
+    mapping(address => bytes32) public merkleRoot;
+    /// @notice Packed array of booleans.
+    mapping(address => mapping(uint256 => uint256)) internal whitelistedBitMap;
+
+    function _calculateDomainSeparator(uint256 chainId) private view returns (bytes32 domainSeperator) {
+        domainSeperator = keccak256(abi.encode(DOMAIN_SEPARATOR_SIGNATURE_HASH, keccak256("WhiteListManager"), chainId, address(this)));
     }
 
-    function DOMAIN_SEPARATOR() public view returns (bytes32) {
-        uint256 chainId;
-        assembly {
-            chainId := chainid()
-        }
-        return chainId == DOMAIN_SEPARATOR_CHAIN_ID ? _DOMAIN_SEPARATOR : _calculateDomainSeparator(chainId);
+    function DOMAIN_SEPARATOR() public view returns (bytes32 domainSeperator) {
+        domainSeperator = block.chainid == DOMAIN_SEPARATOR_CHAIN_ID ? _DOMAIN_SEPARATOR : _calculateDomainSeparator(block.chainid);
     }
 
     constructor() {
-        uint256 chainId;
-        assembly {
-            chainId := chainid()
-        }
-        _DOMAIN_SEPARATOR = _calculateDomainSeparator(DOMAIN_SEPARATOR_CHAIN_ID = chainId);
+        _DOMAIN_SEPARATOR = _calculateDomainSeparator(DOMAIN_SEPARATOR_CHAIN_ID = block.chainid);
     }
 
-    function whitelistUser(address user, bool approved) public {
-        _whitelistUser(msg.sender, user, approved);
+    function whitelistAccount(address user, bool approved) external {
+        _whitelistAccount(msg.sender, user, approved);
     }
 
-    function _whitelistUser(
+    function _whitelistAccount(
         address operator,
-        address user,
+        address account,
         bool approved
     ) private {
-        whitelistedUsers[operator][user] = approved;
-        emit LogWhiteListUser(operator, user, approved);
+        whitelistedAccounts[operator][account] = approved;
+        emit WhiteListAccount(operator, account, approved);
     }
 
-    /// @notice Approves or revokes whitelisting for users
+    /// @notice Approves or revokes whitelisting for accounts.
     /// @param operator The address of the operator that approves or revokes access.
-    /// @param user The address who gains or loses access.
-    /// @param approved If True approves access. If False revokes access.
-    /// @param deadline Time when signature expires to prohibit replays.
-    /// @param v Part of the signature. (See EIP-191)
-    /// @param r Part of the signature. (See EIP-191)
-    /// @param s Part of the signature. (See EIP-191)
+    /// @param account The address who gains or loses access.
+    /// @param approved If 'true', approves access - if 'false', revokes access.
+    /// @param deadline The time at which to expire the signature.
+    /// @param v The recovery byte of the signature.
+    /// @param r Half of the ECDSA signature pair.
+    /// @param s Half of the ECDSA signature pair.
     function setWhitelisting(
         address operator,
-        address user,
+        address account,
         bool approved,
         uint256 deadline,
         uint8 v,
         bytes32 r,
         bytes32 s
-    ) public {
-        // Checks
-        require(user != address(0), "WhiteListMgr: user not set");
-
-        // Also, ecrecover returns address(0) on failure. So we check this, even if the modifier should prevent this:
-        require(operator != address(0), "WhiteListMgr: Operator cannot be 0");
-
-        require(deadline >= block.timestamp && deadline <= (block.timestamp + 1 weeks), "WhiteListMgr: EXPIRED");
+    ) external {
+        // @dev Checks:
+        require(account != address(0), "ACCOUNT_NOT_SET");
+        // - also, ecrecover returns address(0) on failure. So we check this, even if the modifier should prevent it:
+        require(operator != address(0), "OPERATOR_NULL");
+        require(deadline >= block.timestamp && deadline <= (block.timestamp + 1 weeks), "EXPIRED");
 
         bytes32 digest = keccak256(
             abi.encodePacked(
                 EIP191_PREFIX_FOR_EIP712_STRUCTURED_DATA,
                 DOMAIN_SEPARATOR(),
-                keccak256(abi.encode(APPROVAL_SIGNATURE_HASH, user, approved, deadline))
+                keccak256(abi.encode(APPROVAL_SIGNATURE_HASH, account, approved, deadline))
             )
         );
 
         address recoveredAddress = ecrecover(digest, v, r, s);
-        require(recoveredAddress == operator, "WhiteListMgr: Invalid Signature");
-
-        _whitelistUser(operator, user, approved);
+        require(recoveredAddress == operator, "INVALID_SIGNATURE");
+        
+        _whitelistAccount(operator, account, approved);
     }
 
     /// **** WHITELISTING
-    // @dev Adapted from OpenZeppelin utilities and Uniswap merkle distributor.
+    /// @dev Adapted from OpenZeppelin utilities and Uniswap merkle distributor.
     function isWhitelisted(address operator, uint256 index) public view returns (bool success) {
         uint256 whitelistedWordIndex = index / 256;
         uint256 whitelistedBitIndex = index % 256;
@@ -113,7 +98,7 @@ contract WhiteListManager {
         uint256 index,
         address account,
         bytes32[] calldata merkleProof
-    ) public {
+    ) external {
         require(!isWhitelisted(operator, index), "CLAIMED");
         bytes32 node = keccak256(abi.encodePacked(index, account));
         bytes32 computedHash = node;
@@ -132,13 +117,13 @@ contract WhiteListManager {
         uint256 whitelistedWordIndex = index / 256;
         uint256 whitelistedBitIndex = index % 256;
         whitelistedBitMap[operator][whitelistedWordIndex] = whitelistedBitMap[operator][whitelistedWordIndex] | (1 << whitelistedBitIndex);
-        _whitelistUser(operator, account, true);
-        emit LogJoinWithMerkle(operator, index, account);
+        _whitelistAccount(operator, account, true);
+        emit JoinWithMerkle(operator, index, account);
     }
 
-    function setMerkleRoot(bytes32 _merkleRoot) public {
+    function setMerkleRoot(bytes32 _merkleRoot) external {
         // @dev Set the new merkle root.
         merkleRoot[msg.sender] = _merkleRoot;
-        emit LogSetMerkleRoot(msg.sender, _merkleRoot);
+        emit SetMerkleRoot(msg.sender, _merkleRoot);
     }
 }
