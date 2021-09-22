2eb833e755402186dda7aea3fcd8390101480407
Okavango <bretnwood@gmail.com>
2021-09-22 18:48:21 +0300

    Routing multipool test (#134)
    * feat: multipool router test (WIP)
    
    * multirouter test
    
    * feat: more testing setup
    
    * feat: more testing
    
    * feat: more testing setups
    
    * update sdk
    
    * add interfaces
    
    * fix: cleanup params
    
    * stop sort, use references
    
    * routing multipool test fix
    
    * Added function to convert multiroute to
    exactInputParams
    
    * Get paths from any amount of legs
    
    * Updated test to use complex path func
    
    * Tines: multiPoolTest fix
    
    * Added method to generated topology WIP
    
    * Comments
    
    * Added code to generate topology
    
    * Updated topology creation.
    
    * update getComplexPathParamsFromMultiRoute
    
    * renamed function to convertRoute
    
    * Completed test helper functions
    
    * Added logs for debug & removed input params
    from getABCTopology
    
    * Removed contract code from test
    
    * Tines: remove console.log from test
    
    * Removed random generators & made 50% probability of pool type
    
    * Tines: Generate multiple topologies for multirouter
    
    * Tines: Updated multirouter tests
    
    * Tines: Restore big number functoin
    
    * Tines: Tests for Parallel & Serial pools
    
    * Tines: Refactored code to get recipent address
    
    * Tines: real parallel expectation was added
    
    * Tines: Reduced amount in
    
    * Tines: Set random pool reserves
    
    * Tines: removed console log blocks
    
    * Tines: Added undefined check to tests
    
    * Tines: switch to @sushiswap/tines lib
    
    * package-lock deletion
    
    Co-authored-by: technicallyty <48813565+tytech3@users.noreply.github.com>
    Co-authored-by: bayological <paulbayo2002@hotmail.com>

---
+++ test/router/RoutingMultiPool.test.ts
@@ -0,0 +1,185 @@
+import { expect } from "chai";
+import seedrandom from "seedrandom";
+
+import * as testHelper from "./helpers";
+import { areCloseValues, getIntegerRandomValue } from "../utilities";
+
+describe("MultiPool Routing Tests", function () {
+  beforeEach(async function () {
+    this.signer = await testHelper.init();
+    this.gasPrice = 1 * 200 * 1e-9;
+    this.rnd = seedrandom("2");
+  });
+
+  it("Should Test Normal Values with 3 Parallel Pools", async function () {
+    const topology = await testHelper.getAB3VariantTopoplogy(this.rnd);
+
+    const fromToken = topology.tokens[0];
+    const toToken = topology.tokens[1];
+    const baseToken = topology.tokens[1];
+    const [amountIn] = getIntegerRandomValue(30, this.rnd);
+
+    const route = testHelper.createRoute(
+      fromToken,
+      toToken,
+      baseToken,
+      topology,
+      amountIn,
+      this.gasPrice
+    );
+
+    if (route == undefined) {
+      throw "Failed to get route";
+    }
+
+    expect(route.legs.length).equal(3);
+
+    const routerParams = testHelper.getComplexPathParams(
+      route,
+      this.signer.address,
+      fromToken.address,
+      toToken.address
+    );
+
+    const amountOutPoolBN = await testHelper.executeComplexPath(
+      routerParams,
+      toToken.address
+    );
+
+    expect(
+      areCloseValues(
+        route.amountOut,
+        parseInt(amountOutPoolBN.toString()),
+        1e-14
+      )
+    ).to.equal(true, "predicted amount did not equal actual swapped amount");
+  });
+
+  it("Should Test Normal Values with 2 Parallel Pools", async function () {
+    const topology = await testHelper.getAB2VariantTopoplogy(this.rnd);
+
+    const fromToken = topology.tokens[0];
+    const toToken = topology.tokens[1];
+    const baseToken = topology.tokens[1];
+    const [amountIn] = getIntegerRandomValue(30, this.rnd);
+
+    const route = testHelper.createRoute(
+      fromToken,
+      toToken,
+      baseToken,
+      topology,
+      amountIn,
+      this.gasPrice
+    );
+
+    if (route == undefined) {
+      throw "Failed to get route";
+    }
+
+    expect(route.legs.length).equal(2);
+
+    const routerParams = testHelper.getComplexPathParams(
+      route,
+      this.signer.address,
+      fromToken.address,
+      toToken.address
+    );
+
+    const amountOutPoolBN = await testHelper.executeComplexPath(
+      routerParams,
+      toToken.address
+    );
+
+    expect(
+      areCloseValues(
+        route.amountOut,
+        parseInt(amountOutPoolBN.toString()),
+        1e-14
+      )
+    ).to.equal(true, "predicted amount did not equal actual swapped amount");
+  });
+
+  it("Should Test Normal Values With 2 Serial Pools", async function () {
+    const topology = await testHelper.getABCTopoplogy(this.rnd);
+
+    const fromToken = topology.tokens[0];
+    const toToken = topology.tokens[2];
+    const baseToken = topology.tokens[1];
+    const [amountIn] = getIntegerRandomValue(20, this.rnd);
+
+    const route = testHelper.createRoute(
+      fromToken,
+      toToken,
+      baseToken,
+      topology,
+      amountIn,
+      this.gasPrice
+    );
+
+    if (route == undefined) {
+      throw "Failed to get route";
+    }
+
+    const routerParams = testHelper.getComplexPathParams(
+      route,
+      this.signer.address,
+      fromToken.address,
+      toToken.address
+    );
+
+    const amountOutPoolBN = await testHelper.executeComplexPath(
+      routerParams,
+      toToken.address
+    );
+
+    expect(
+      areCloseValues(
+        route.amountOut,
+        parseInt(amountOutPoolBN.toString()),
+        1e-14
+      )
+    ).to.equal(true, "predicted amount did not equal actual swapped amount");
+  });
+
+  it("Should Test Normal Values With 3 Serial Pools", async function () {
+    const topology = await testHelper.getABCDTopoplogy(this.rnd);
+
+    const fromToken = topology.tokens[0];
+    const toToken = topology.tokens[2];
+    const baseToken = topology.tokens[1];
+    const [amountIn] = getIntegerRandomValue(20, this.rnd);
+
+    const route = testHelper.createRoute(
+      fromToken,
+      toToken,
+      baseToken,
+      topology,
+      amountIn,
+      this.gasPrice
+    );
+
+    if (route == undefined) {
+      throw "Failed to get route";
+    }
+
+    const routerParams = testHelper.getComplexPathParams(
+      route,
+      this.signer.address,
+      fromToken.address,
+      toToken.address
+    );
+
+    const amountOutPoolBN = await testHelper.executeComplexPath(
+      routerParams,
+      toToken.address
+    );
+
+    expect(
+      areCloseValues(
+        route.amountOut,
+        parseInt(amountOutPoolBN.toString()),
+        1e-14
+      )
+    ).to.equal(true, "predicted amount did not equal actual swapped amount");
+  });
+});
