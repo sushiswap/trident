c405f3402a1ed336244053f8186742d2da5975e9
gasperbr <42702599+gasperbr@users.noreply.github.com>
2021-09-30 01:10:07 +0200

    update encoding in factory to exclude extra bytes (#193)

--- contracts/pool/concentrated/ConcentratedLiquidityPoolFactory.sol
+++ contracts/pool/concentrated/ConcentratedLiquidityPoolFactory.sol
@@ -17,8 +17,10 @@ contract ConcentratedLiquidityPoolFactory is PoolDeployer {
         );
         if (tokenA > tokenB) {
             (tokenA, tokenB) = (tokenB, tokenA);
-            _deployData = abi.encode(tokenA, tokenB, swapFee, price, tickSpacing);
         }
+        // @dev Strips any extra data.
+        _deployData = abi.encode(tokenA, tokenB, swapFee, price, tickSpacing);
+
         address[] memory tokens = new address[](2);
         tokens[0] = tokenA;
         tokens[1] = tokenB;
