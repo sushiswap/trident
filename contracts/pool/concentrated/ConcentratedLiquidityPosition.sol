a47c423619938fba6592381f397d07e7c3bac421
gasperbr <42702599+gasperbr@users.noreply.github.com>
2021-09-29 03:35:28 +0200

    Bugfix - don't allow calling burn with amount greater then we own (#182)

--- contracts/pool/concentrated/ConcentratedLiquidityPosition.sol
+++ contracts/pool/concentrated/ConcentratedLiquidityPosition.sol
@@ -59,8 +59,10 @@ abstract contract ConcentratedLiquidityPosition is TridentNFT {
     ) external {
         require(msg.sender == ownerOf[tokenId], "NOT_ID_OWNER");
         Position storage position = positions[tokenId];
-        bytes memory burnData = abi.encode(position.lower, position.upper, amount, recipient, unwrapBento);
-        position.pool.burn(burnData);
+        if (position.liquidity < amount) amount = position.liquidity;
+
+        position.pool.burn(abi.encode(position.lower, position.upper, amount, recipient, unwrapBento));
+
         if (amount < position.liquidity) {
             position.liquidity -= amount;
         } else {
