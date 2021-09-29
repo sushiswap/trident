7b031b53810c5dbeefd471db9d0a256284f7a9bf
Sarang Parikh <sarangparikh22@gmail.com>
2021-09-30 01:55:08 +0530

    updated max fee for pool to 10%

--- contracts/pool/concentrated/ConcentratedLiquidityPool.sol
+++ contracts/pool/concentrated/ConcentratedLiquidityPool.sol
@@ -30,11 +30,11 @@ contract ConcentratedLiquidityPool is IPool {
     // 100 tickSpacing -> 2% between ticks
     bytes32 public constant override poolIdentifier = "Trident:ConcentratedLiquidity";
 
-    uint24 internal constant MAX_FEE = 10000; /// @dev 100%.
+    uint24 internal constant MAX_FEE = 100000; /// @dev Maximum swap fee is 10%.
 
     uint128 internal immutable MAX_TICK_LIQUIDITY;
     uint24 internal immutable tickSpacing;
-    uint24 internal immutable swapFee; /// @dev 1000 corresponds to 0.1% fee.
+    uint24 internal immutable swapFee; /// @dev 1000 corresponds to 0.1% fee. Fee is measured in pips.
 
     address internal immutable barFeeTo;
     IBentoBoxMinimal internal immutable bento;
