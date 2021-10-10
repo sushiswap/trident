dc4eae6ed7cbc1be0ee16cac95a1cd35c7b5e459
gasper <gasper@sushi.com>
2021-10-11 00:49:51 +0200

    remove unused interface

--- contracts/interfaces/IMigrator.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-pragma solidity >=0.8.0;
-
-/// @notice Trident pool migrator interface for legacy SushiSwap.
-interface IMigrator {
-    /// @notice Returns desired amount of liquidity tokens for migration.
-    function desiredLiquidity() external view returns (uint256);
-}
