1dbfbbd09096ad994e9a0c78e89491a6faac3c8f
bayological <paulbayo2002@hotmail.com>
2021-10-10 22:11:21 -0500

    Tines: increase liquidity

--- test/router/helpers/price.ts
+++ test/router/helpers/price.ts
@@ -1,7 +1,7 @@
 import { getRandom } from "./random";
 
-const MIN_TOKEN_PRICE = 1e-6
-const MAX_TOKEN_PRICE = 1e6
+const MIN_TOKEN_PRICE = 1e-4
+const MAX_TOKEN_PRICE = 1e4
   
 export function getTokenPrice(rnd: () => number) {
     const price = getRandom(rnd, MIN_TOKEN_PRICE, MAX_TOKEN_PRICE)
