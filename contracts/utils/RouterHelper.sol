b4260eb00cbfa73212154c33c8361f8993551ecd
gasper <gasper@sushi.com>
2021-10-13 03:24:26 +0200

    Add batch and permit abstract contracts

--- contracts/utils/RouterHelper.sol
+++ contracts/utils/RouterHelper.sol
@@ -18,7 +18,7 @@ contract RouterHelper {
     /// @notice Provides batch function calls for this contract and returns the data from all of them if they all succeed.
     /// Adapted from https://github.com/Uniswap/uniswap-v3-periphery/blob/main/contracts/base/Multicall.sol, License-Identifier: GPL-2.0-or-later.
     /// @dev The `msg.value` should not be trusted for any method callable from this function.
-    /// @dev Uses a modified version of the batch function - preventing multiple calls of the swap functions
+    /// @dev Uses a modified version of the batch function - preventing multiple calls of the single input swap functions
     /// @param data ABI-encoded params for each of the calls to make to this contract.
     /// @return results The results from each of the calls passed in via `data`.
     function batch(bytes[] calldata data) external payable returns (bytes[] memory results) {
@@ -38,7 +38,7 @@ contract RouterHelper {
 
             (bool success, bytes memory result) = address(this).delegatecall(data[i]);
             if (!success) {
-                // @dev Next 5 lines from https://ethereum.stackexchange.com/a/83577.
+                // Next 5 lines from https://ethereum.stackexchange.com/a/83577.
                 if (result.length < 68) revert();
                 assembly {
                     result := add(result, 0x04)
