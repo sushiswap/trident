1e74dfe14c071cc6c2897411fb9bd76cfea58546
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-10-15 11:11:39 -0400

    Update FullMath.sol
    avoid natspec within functions

--- contracts/libraries/concentratedPool/FullMath.sol
+++ contracts/libraries/concentratedPool/FullMath.sol
@@ -18,19 +18,19 @@ library FullMath {
         uint256 denominator
     ) internal pure returns (uint256 result) {
         unchecked {
-            // @dev 512-bit multiply [prod1 prod0] = a * b.
+            // 512-bit multiply [prod1 prod0] = a * b.
             // Compute the product mod 2**256 and mod 2**256 - 1,
             // then use the Chinese Remainder Theorem to reconstruct
             // the 512 bit result. The result is stored in two 256
             // variables such that product = prod1 * 2**256 + prod0.
-            uint256 prod0; // @dev Least significant 256 bits of the product.
-            uint256 prod1; // @dev Most significant 256 bits of the product.
+            uint256 prod0; // Least significant 256 bits of the product.
+            uint256 prod1; // Most significant 256 bits of the product.
             assembly {
                 let mm := mulmod(a, b, not(0))
                 prod0 := mul(a, b)
                 prod1 := sub(sub(mm, prod0), lt(mm, prod0))
             }
-            // @dev Handle non-overflow cases, 256 by 256 division.
+            // Handle non-overflow cases, 256 by 256 division.
             if (prod1 == 0) {
                 require(denominator > 0);
                 assembly {
@@ -38,49 +38,49 @@ library FullMath {
                 }
                 return result;
             }
-            // @dev Make sure the result is less than 2**256 -
+            // Make sure the result is less than 2**256 -
             // also prevents denominator == 0.
             require(denominator > prod1);
             ///////////////////////////////////////////////
             // 512 by 256 division.
             ///////////////////////////////////////////////
-            // @dev Make division exact by subtracting the remainder from [prod1 prod0] -
+            // Make division exact by subtracting the remainder from [prod1 prod0] -
             // compute remainder using mulmod.
             uint256 remainder;
             assembly {
                 remainder := mulmod(a, b, denominator)
             }
-            // @dev Subtract 256 bit number from 512 bit number.
+            // Subtract 256 bit number from 512 bit number.
             assembly {
                 prod1 := sub(prod1, gt(remainder, prod0))
                 prod0 := sub(prod0, remainder)
             }
-            // @dev Factor powers of two out of denominator -
+            // Factor powers of two out of denominator -
             // compute largest power of two divisor of denominator
             // (always >= 1).
             uint256 twos = uint256(-int256(denominator)) & denominator;
-            // @dev Divide denominator by power of two.
+            // Divide denominator by power of two.
             assembly {
                 denominator := div(denominator, twos)
             }
-            // @dev Divide [prod1 prod0] by the factors of two.
+            // Divide [prod1 prod0] by the factors of two.
             assembly {
                 prod0 := div(prod0, twos)
             }
-            // @dev Shift in bits from prod1 into prod0. For this we need
+            // Shift in bits from prod1 into prod0. For this we need
             // to flip `twos` such that it is 2**256 / twos -
             // if twos is zero, then it becomes one.
             assembly {
                 twos := add(div(sub(0, twos), twos), 1)
             }
             prod0 |= prod1 * twos;
-            // @dev Invert denominator mod 2**256 -
+            // Invert denominator mod 2**256 -
             // now that denominator is an odd number, it has an inverse
             // modulo 2**256 such that denominator * inv = 1 mod 2**256.
             // Compute the inverse by starting with a seed that is correct
             // for four bits. That is, denominator * inv = 1 mod 2**4.
             uint256 inv = (3 * denominator) ^ 2;
-            // @dev Now use Newton-Raphson iteration to improve the precision.
+            // Now use Newton-Raphson iteration to improve the precision.
             // Thanks to Hensel's lifting lemma, this also works in modular
             // arithmetic, doubling the correct bits in each step.
             inv *= 2 - denominator * inv; // @dev Inverse mod 2**8.
@@ -89,7 +89,7 @@ library FullMath {
             inv *= 2 - denominator * inv; // @dev Inverse mod 2**64.
             inv *= 2 - denominator * inv; // @dev Inverse mod 2**128.
             inv *= 2 - denominator * inv; // @dev Inverse mod 2**256.
-            // @dev Because the division is now exact we can divide by multiplying
+            // Because the division is now exact we can divide by multiplying
             // with the modular inverse of denominator. This will give us the
             // correct result modulo 2**256. Since the precoditions guarantee
             // that the outcome is less than 2**256, this is the final result.
