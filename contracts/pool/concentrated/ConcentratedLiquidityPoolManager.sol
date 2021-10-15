dd5ed998176b1428643cf9009cb3ce337f750e22
gasper <gasper@sushi.com>
2021-10-15 04:49:49 +0200

    wip - split position contract and staker

--- contracts/pool/concentrated/ConcentratedLiquidityPoolManager.sol
+++ contracts/pool/concentrated/ConcentratedLiquidityPoolManager.sol
@@ -2,168 +2,174 @@
 
 pragma solidity >=0.8.0;
 
-import "../../interfaces/IConcentratedLiquidityPool.sol";
-import "./ConcentratedLiquidityPosition.sol";
-import "../../libraries/concentratedPool/Ticks.sol";
-import "hardhat/console.sol";
+import "../../interfaces/IBentoBoxMinimal.sol";
+import "../../interfaces/IConcentratedLiquidityPoolManager.sol";
+import "../../interfaces/IMasterDeployer.sol";
+import "../../interfaces/ITridentRouter.sol";
+import "../../libraries/concentratedPool/FullMath.sol";
+import "../../libraries/concentratedPool/TickMath.sol";
+import "../../libraries/concentratedPool/DyDxMath.sol";
+import "../../utils/TridentBatchable.sol";
+import "./TridentNFT.sol";
 
 /// @notice Trident Concentrated Liquidity Pool periphery contract that combines non-fungible position management and staking.
-contract ConcentratedLiquidityPoolManager is ConcentratedLiquidityPosition {
-    event AddIncentive(IConcentratedLiquidityPool indexed pool, uint256 indexed incentiveId, address indexed rewardToken);
-    event Subscribe(uint256 indexed positionId, uint256 indexed incentiveId);
-    event ClaimReward(uint256 indexed positionId, uint256 indexed incentiveId, address indexed recipient, uint96 amount);
-    event ReclaimIncentive(IConcentratedLiquidityPool indexed pool, uint256 indexed incentiveId, uint256 amount);
+contract ConcentratedLiquidityPosition is IConcentratedLiquidityPoolManager, TridentNFT {
+    event Mint(address indexed pool, address indexed recipient, uint256 indexed positionId);
+    event Burn(address indexed pool, address indexed owner, uint256 indexed positionId);
 
-    struct Incentive {
-        address owner;
-        address token;
-        uint32 startTime;
-        uint32 endTime;
-        uint32 expiry;
-        uint160 secondsClaimed; // @dev x128.
-        uint96 rewardsUnclaimed;
+    IBentoBoxMinimal public immutable bento;
+    IMasterDeployer public immutable masterDeployer;
+
+    mapping(uint256 => Position) public positions;
+
+    constructor(address _masterDeployer) {
+        /// @dev Don't need to check _masterDeployer != address(0) as we make a call to it.
+        masterDeployer = IMasterDeployer(_masterDeployer);
+        IBentoBoxMinimal _bento = IBentoBoxMinimal(IMasterDeployer(_masterDeployer).bento());
+        _bento.registerProtocol();
+        bento = _bento;
     }
 
-    struct Stake {
-        uint160 secondsGrowthInsideLast; // @dev x128.
-        bool initialized;
+    function positionMintCallback(
+        address recipient,
+        int24 lower,
+        int24 upper,
+        uint128 amount,
+        uint256 feeGrowthInside0,
+        uint256 feeGrowthInside1
+    ) external returns (uint256 positionId) {
+        require(IMasterDeployer(masterDeployer).pools(msg.sender), "NOT_POOL");
+        positions[totalSupply] = Position(IConcentratedLiquidityPool(msg.sender), amount, lower, upper, feeGrowthInside0, feeGrowthInside1);
+        positionId = totalSupply;
+        _mint(recipient);
+        emit Mint(msg.sender, recipient, positionId);
     }
 
-    mapping(IConcentratedLiquidityPool => uint256) public incentiveCount;
-    mapping(IConcentratedLiquidityPool => mapping(uint256 => Incentive)) public incentives;
-    /// @dev When subscribing to an incentive we take a snapshot of the position secondsGrowth accumulator.
-    /// @dev positionId to incentiveId to position's secondsGrowth snapshot mapping.
-    mapping(uint256 => mapping(uint256 => Stake)) public stakes;
-
-    constructor(address _masterDeployer) ConcentratedLiquidityPosition(_masterDeployer) {}
-
-    function addIncentive(IConcentratedLiquidityPool pool, Incentive memory incentive) public {
-        uint32 current = uint32(block.timestamp);
-        require(current <= incentive.startTime, "ALREADY_STARTED");
-        require(incentive.startTime < incentive.endTime, "START_PAST_END");
-        require(incentive.endTime + 90 days < incentive.expiry, "END_PAST_BUFFER");
-        require(incentive.rewardsUnclaimed != 0, "NO_REWARDS");
-        incentive.secondsClaimed = 0;
-        incentives[pool][incentiveCount[pool]++] = incentive;
-        _transfer(incentive.token, msg.sender, address(this), incentive.rewardsUnclaimed, false);
-        emit AddIncentive(pool, incentiveCount[pool], incentive.token);
-    }
-
-    /// @dev Withdraws any unclaimed incentive rewards.
-    function reclaimIncentive(
-        IConcentratedLiquidityPool pool,
-        uint256 incentiveId,
-        address receiver,
-        uint96 amount,
-        bool unwrapBento
-    ) public {
-        Incentive storage incentive = incentives[pool][incentiveId];
-        require(incentive.owner == msg.sender, "NOT_OWNER");
-        require(incentive.expiry < block.timestamp, "EXPIRED");
-        require(incentive.rewardsUnclaimed >= amount, "ALREADY_CLAIMED");
-        incentive.rewardsUnclaimed -= uint96(amount);
-        _transfer(incentive.token, address(this), receiver, amount, unwrapBento);
-        emit ReclaimIncentive(pool, incentiveId, amount);
-    }
-
-    /// @dev Subscribes a non-fungible position token to an incentive.
-    function subscribe(uint256 positionId, uint256[] calldata incentiveId) external {
-        require(ownerOf[positionId] == msg.sender, "OWNER");
-        Position memory position = positions[positionId];
-        IConcentratedLiquidityPool pool = position.pool;
-        require(position.liquidity != 0, "INACTIVE");
-        Stake memory stakeData = Stake(uint160(rangeSecondsInside(pool, position.lower, position.upper)), true);
-        for (uint256 i; i < incentiveId.length; i++) {
-            Incentive memory incentive = incentives[pool][incentiveId[i]];
-            Stake storage stake = stakes[positionId][incentiveId[i]];
-            require(stake.secondsGrowthInsideLast == 0, "SUBSCRIBED");
-            require(block.timestamp >= incentive.startTime && block.timestamp < incentive.endTime, "INACTIVE_INCENTIVE");
-            stakes[positionId][incentiveId[i]] = stakeData;
-            emit Subscribe(positionId, incentiveId[i]);
-        }
-    }
-
-    function claimRewards(
-        uint256 positionId,
-        uint256[] memory incentiveIds,
+    function burn(
+        uint256 tokenId,
+        uint128 amount,
         address recipient,
         bool unwrapBento
-    ) public {
-        require(ownerOf[positionId] == msg.sender, "OWNER");
+    ) external {
+        require(msg.sender == ownerOf[tokenId], "NOT_ID_OWNER");
+        Position storage position = positions[tokenId];
 
-        Position memory position = positions[positionId];
-        IConcentratedLiquidityPool pool = position.pool;
+        if (amount < position.liquidity) {
+            (uint256 currentPrice, ) = position.pool.getPriceAndNearestTicks();
+            uint160 priceLower = TickMath.getSqrtRatioAtTick(position.lower);
+            uint160 priceUpper = TickMath.getSqrtRatioAtTick(position.upper);
 
-        uint256 currentSecondsGrowth = rangeSecondsInside(pool, position.lower, position.upper);
-
-        for (uint256 i = 0; i < incentiveIds.length; i++) {
-            Incentive storage incentive = incentives[pool][incentiveIds[i]];
-            Stake storage stake = stakes[positionId][incentiveIds[i]];
-
-            require(stake.initialized, "UNINITIALIZED");
-
-            uint256 rewards;
-            uint256 secondsInside;
+            uint256 feeAmount0;
+            uint256 feeAmount1;
 
             {
-                uint256 secondsGrowth = currentSecondsGrowth - stake.secondsGrowthInsideLast;
-                uint256 maxTime = block.timestamp < incentive.endTime ? incentive.endTime : block.timestamp;
-                uint256 secondsUnclaimed = ((maxTime - incentive.startTime) << 128) - incentive.secondsClaimed;
-                secondsInside = secondsGrowth * position.liquidity; // secondsGrowth is multiplied by 2**128
-                rewards = (incentive.rewardsUnclaimed * secondsInside) / secondsUnclaimed; // 2**128 cancels out
+                (uint256 feeGrowthInside0, uint256 feeGrowthInside1) = position.pool.rangeFeeGrowth(position.lower, position.upper);
+                feeAmount0 = FullMath.mulDiv(
+                    feeGrowthInside0 - position.feeGrowthInside0,
+                    position.liquidity,
+                    0x100000000000000000000000000000000
+                );
+
+                feeAmount1 = FullMath.mulDiv(
+                    feeGrowthInside1 - position.feeGrowthInside1,
+                    position.liquidity,
+                    0x100000000000000000000000000000000
+                );
+
+                position.feeGrowthInside0 = feeGrowthInside0;
+                position.feeGrowthInside1 = feeGrowthInside1;
             }
 
-            stake.secondsGrowthInsideLast = uint160(currentSecondsGrowth);
-            incentive.secondsClaimed += uint160(secondsInside);
-            incentive.rewardsUnclaimed -= uint96(rewards);
+            (uint256 token0Amount, uint256 token1Amount) = DyDxMath.getAmountsForLiquidity(
+                priceLower,
+                priceUpper,
+                currentPrice,
+                amount,
+                false
+            );
 
-            _transfer(incentive.token, address(this), recipient, rewards, unwrapBento);
+            IPool.TokenAmount[] memory withdrawAmounts = position.pool.burn(
+                abi.encode(position.lower, position.upper, amount, address(this), false)
+            );
 
-            emit ClaimReward(positionId, incentiveIds[i], recipient, uint96(rewards));
+            position.liquidity -= amount;
+
+            _transfer(withdrawAmounts[0].token, address(this), recipient, token0Amount + feeAmount0, unwrapBento);
+            _transfer(withdrawAmounts[1].token, address(this), recipient, token1Amount + feeAmount1, unwrapBento);
+        } else {
+            collect(tokenId, recipient, unwrapBento);
+
+            position.pool.burn(abi.encode(position.lower, position.upper, position.liquidity, recipient, unwrapBento));
+
+            delete positions[tokenId];
+
+            _burn(tokenId);
         }
+
+        emit Burn(address(position.pool), msg.sender, tokenId);
     }
 
-    function getReward(uint256 positionId, uint256 incentiveId) public view returns (uint256 rewards, uint256 secondsInside) {
-        Position memory position = positions[positionId];
-        IConcentratedLiquidityPool pool = position.pool;
-        Incentive memory incentive = incentives[pool][positionId];
-        Stake memory stake = stakes[positionId][incentiveId];
-        if (stake.initialized) {
-            uint256 secondsGrowth = rangeSecondsInside(pool, position.lower, position.upper) - stake.secondsGrowthInsideLast;
-            secondsInside = secondsGrowth * position.liquidity;
-            uint256 maxTime = block.timestamp < incentive.endTime ? incentive.endTime : block.timestamp;
-            uint256 secondsUnclaimed = ((maxTime - incentive.startTime) << 128) - incentive.secondsClaimed;
-            rewards = (incentive.rewardsUnclaimed * secondsInside) / secondsUnclaimed;
+    function collect(
+        uint256 tokenId,
+        address recipient,
+        bool unwrapBento
+    ) public returns (uint256 token0amount, uint256 token1amount) {
+        require(msg.sender == ownerOf[tokenId], "NOT_ID_OWNER");
+
+        Position storage position = positions[tokenId];
+
+        (address token0, address token1) = _getAssets(position.pool);
+
+        {
+            (uint256 feeGrowthInside0, uint256 feeGrowthInside1) = position.pool.rangeFeeGrowth(position.lower, position.upper);
+            token0amount = FullMath.mulDiv(
+                feeGrowthInside0 - position.feeGrowthInside0,
+                position.liquidity,
+                0x100000000000000000000000000000000
+            );
+            token1amount = FullMath.mulDiv(
+                feeGrowthInside1 - position.feeGrowthInside1,
+                position.liquidity,
+                0x100000000000000000000000000000000
+            );
+
+            position.feeGrowthInside0 = feeGrowthInside0;
+            position.feeGrowthInside1 = feeGrowthInside1;
         }
+
+        uint256 balance0 = bento.balanceOf(token0, address(this));
+        uint256 balance1 = bento.balanceOf(token1, address(this));
+        if (balance0 < token0amount || balance1 < token1amount) {
+            (uint256 amount0fees, uint256 amount1fees) = position.pool.collect(position.lower, position.upper, address(this), false);
+
+            uint256 newBalance0 = amount0fees + balance0;
+            uint256 newBalance1 = amount1fees + balance1;
+
+            /// @dev Rounding errors due to frequent claiming of other users in the same position may cost us some raw
+            if (token0amount > newBalance0) token0amount = newBalance0;
+            if (token1amount > newBalance1) token1amount = newBalance1;
+        }
+        _transfer(token0, address(this), recipient, token0amount, unwrapBento);
+        _transfer(token1, address(this), recipient, token1amount, unwrapBento);
     }
 
-    /// @dev Calculates the "seconds per liquidity" accumulator for a range.
-    function rangeSecondsInside(
-        IConcentratedLiquidityPool pool,
-        int24 lowerTick,
-        int24 upperTick
-    ) public view returns (uint256 secondsInside) {
-        (, int24 currentTick) = pool.getPriceAndNearestTicks();
+    function _getAssets(IConcentratedLiquidityPool pool) internal view returns (address token0, address token1) {
+        address[] memory pair = pool.getAssets();
+        token0 = pair[0];
+        token1 = pair[1];
+    }
 
-        Ticks.Tick memory lower = pool.ticks(lowerTick);
-        Ticks.Tick memory upper = pool.ticks(upperTick);
-
-        (uint256 secondsGrowthGlobal, ) = pool.getSecondsGrowthAndLastObservation();
-        uint256 secondsBelow;
-        uint256 secondsAbove;
-
-        if (lowerTick <= currentTick) {
-            secondsBelow = lower.secondsGrowthOutside;
+    function _transfer(
+        address token,
+        address from,
+        address to,
+        uint256 shares,
+        bool unwrapBento
+    ) internal {
+        if (unwrapBento) {
+            bento.withdraw(token, from, to, 0, shares);
         } else {
-            secondsBelow = secondsGrowthGlobal - lower.secondsGrowthOutside;
+            bento.transfer(token, from, to, shares);
         }
-
-        if (currentTick < upperTick) {
-            secondsAbove = upper.secondsGrowthOutside;
-        } else {
-            secondsAbove = secondsGrowthGlobal - upper.secondsGrowthOutside;
-        }
-
-        secondsInside = secondsGrowthGlobal - secondsBelow - secondsAbove;
     }
 }
