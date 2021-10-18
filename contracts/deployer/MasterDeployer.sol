0daf57c18e7f60952559db5aacfacab332d934fe
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-10-18 13:17:19 -0400

    Update MasterDeployer.sol
    add uniqueness check via pool id

--- contracts/deployer/MasterDeployer.sol
+++ contracts/deployer/MasterDeployer.sol
@@ -2,6 +2,7 @@
 
 pragma solidity >=0.8.0;
 
+import "../interfaces/IPool.sol";
 import "../interfaces/IPoolFactory.sol";
 import "../utils/TridentOwnable.sol";
 
@@ -16,11 +17,17 @@ contract MasterDeployer is TridentOwnable {
     uint256 public barFee;
     address public immutable barFeeTo;
     address public immutable bento;
+    address[] public factoryList;
 
     uint256 internal constant MAX_FEE = 10000; // @dev 100%.
 
     mapping(address => bool) public pools;
-    mapping(address => bool) public whitelistedFactories;
+    mapping(address => Type) public factories;
+    
+    struct Type {
+        bool whitelisted;
+        bytes32 identifier;
+    }
 
     constructor(
         uint256 _barFee,
@@ -37,19 +44,24 @@ contract MasterDeployer is TridentOwnable {
     }
 
     function deployPool(address _factory, bytes calldata _deployData) external returns (address pool) {
-        require(whitelistedFactories[_factory], "FACTORY_NOT_WHITELISTED");
+        require(factories[_factory].whitelisted, "FACTORY_NOT_WHITELISTED");
         pool = IPoolFactory(_factory).deployPool(_deployData);
         pools[pool] = true;
         emit DeployPool(_factory, pool, _deployData);
     }
 
     function addToWhitelist(address _factory) external onlyOwner {
-        whitelistedFactories[_factory] = true;
+        bytes32 identifier = IPool(_factory).poolIdentifier();
+        for (uint256 i = 0; i < factoryList.length; i++) {
+            require(identifier != factories[factoryList[i]].identifier, "FACTORY_NOT_UNIQUE");
+        }
+        factories[_factory] = Type(true, identifier);
+        factoryList.push(_factory);
         emit AddToWhitelist(_factory);
     }
 
     function removeFromWhitelist(address _factory) external onlyOwner {
-        whitelistedFactories[_factory] = false;
+        factories[_factory].whitelisted = false;
         emit RemoveFromWhitelist(_factory);
     }
 
