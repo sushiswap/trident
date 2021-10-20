62c66d95321e6623f59365abe2f74eaaa6c12ee6
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-10-19 23:54:59 -0400

    Update IndexPoolFactory.sol
    nit comments

--- contracts/pool/IndexPoolFactory.sol
+++ contracts/pool/IndexPoolFactory.sol
@@ -15,10 +15,10 @@ contract IndexPoolFactory is PoolDeployer {
     function deployPool(bytes memory _deployData) external returns (address pool) {
         (address[] memory tokens, uint136[] memory weights, uint256 swapFee) = abi.decode(_deployData, (address[], uint136[], uint256));
 
-        // @dev Strips any extra data.
+        // Strips any extra data.
         _deployData = abi.encode(tokens, weights, swapFee);
 
-        // @dev Salt is not actually needed since `_deployData` is part of creationCode and already contains the salt.
+        // Salt is not actually needed since `_deployData` is part of creationCode and already contains the salt.
         bytes32 salt = keccak256(_deployData);
         pool = address(new IndexPool{salt: salt}(_deployData, masterDeployer));
         _registerPool(pool, tokens, salt);
