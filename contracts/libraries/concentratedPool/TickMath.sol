6ee6147ef61a65403d5f91ffe1e6df60e6376175
gasperbr <42702599+gasperbr@users.noreply.github.com>
2021-12-02 00:31:26 +0100

    Update contracts/libraries/concentratedPool/TickMath.sol

--- contracts/libraries/concentratedPool/TickMath.sol
+++ contracts/libraries/concentratedPool/TickMath.sol
@@ -2,7 +2,6 @@
 
 pragma solidity >=0.8.0;
 
-import "./SafeCast.sol";
 
 /// @notice Math library for computing sqrt price for ticks of size 1.0001, i.e., sqrt(1.0001^tick) as fixed point Q64.96 numbers - supports
 /// prices between 2**-128 and 2**128 - 1.
