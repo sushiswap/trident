8343058f881435700cadcc14ec820a5db7dddde8
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-12-28 11:34:26 -0500

    Update RouterHelper.sol
    use low level assembly for ETH & token transfers to optimize gas costs

--- contracts/utils/RouterHelper.sol
+++ contracts/utils/RouterHelper.sol
@@ -89,13 +89,29 @@ contract RouterHelper is TridentPermit {
     /// @param token Address of ERC-20 token.
     /// @param recipient Account to send tokens to.
     /// @param amount Token amount to send.
+    /// @dev Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/SafeTransferLib.sol).
     function safeTransfer(
         address token,
         address recipient,
         uint256 amount
     ) internal {
-        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, recipient, amount)); // @dev transfer(address,uint256).
-        require(success && (data.length == 0 || abi.decode(data, (bool))), "TRANSFER_FAILED");
+        bool callStatus;
+
+        assembly {
+            // Get a pointer to some free memory.
+            let freeMemoryPointer := mload(0x40)
+
+            // Write the abi-encoded calldata to memory piece by piece:
+            mstore(freeMemoryPointer, 0xa9059cbb00000000000000000000000000000000000000000000000000000000) // Begin with the function selector.
+            mstore(add(freeMemoryPointer, 4), and(recipient, 0xffffffffffffffffffffffffffffffffffffffff)) // Mask and append the "to" argument.
+            mstore(add(freeMemoryPointer, 36), amount) // Finally append the "amount" argument. No mask as it's a full 32 byte value.
+
+            // Call the token and store if it succeeded or not.
+            // We use 68 because the calldata length is 4 + 32 * 2.
+            callStatus := call(gas(), token, 0, freeMemoryPointer, 68, 0, 0)
+        }
+
+        require(didLastOptionalReturnCallSucceed(callStatus), "TRANSFER_FAILED");
     }
 
     /// @notice Provides 'safe' ERC-20 {transferFrom} for tokens that don't consistently return true/false.
@@ -103,14 +119,31 @@ contract RouterHelper is TridentPermit {
     /// @param sender Account to send tokens from.
     /// @param recipient Account to send tokens to.
     /// @param amount Token amount to send.
+    /// @dev Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/SafeTransferLib.sol).
     function safeTransferFrom(
         address token,
         address sender,
         address recipient,
         uint256 amount
     ) internal {
-        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, sender, recipient, amount)); // @dev transferFrom(address,address,uint256).
-        require(success && (data.length == 0 || abi.decode(data, (bool))), "TRANSFER_FROM_FAILED");
+        bool callStatus;
+
+        assembly {
+            // Get a pointer to some free memory.
+            let freeMemoryPointer := mload(0x40)
+
+            // Write the abi-encoded calldata to memory piece by piece:
+            mstore(freeMemoryPointer, 0x23b872dd00000000000000000000000000000000000000000000000000000000) // Begin with the function selector.
+            mstore(add(freeMemoryPointer, 4), and(sender, 0xffffffffffffffffffffffffffffffffffffffff)) // Mask and append the "from" argument.
+            mstore(add(freeMemoryPointer, 36), and(recipient, 0xffffffffffffffffffffffffffffffffffffffff)) // Mask and append the "to" argument.
+            mstore(add(freeMemoryPointer, 68), amount) // Finally append the "amount" argument. No mask as it's a full 32 byte value.
+
+            // Call the token and store if it succeeded or not.
+            // We use 100 because the calldata length is 4 + 32 * 3.
+            callStatus := call(gas(), token, 0, freeMemoryPointer, 100, 0, 0)
+        }
+
+        require(didLastOptionalReturnCallSucceed(callStatus), "TRANSFER_FROM_FAILED");
     }
 
     /// @notice Provides low-level `wETH` {withdraw}.
@@ -123,18 +156,56 @@ contract RouterHelper is TridentPermit {
     /// @notice Provides 'safe' ETH transfer.
     /// @param recipient Account to send ETH to.
     /// @param amount ETH amount to send.
+    /// @dev Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/SafeTransferLib.sol).
     function safeTransferETH(address recipient, uint256 amount) internal {
-        (bool success, ) = recipient.call{value: amount}("");
-        require(success, "ETH_TRANSFER_FAILED");
+        bool callStatus;
+
+        assembly {
+            // Transfer the ETH and store if it succeeded or not.
+            callStatus := call(gas(), recipient, amount, 0, 0, 0, 0)
+        }
+
+        require(callStatus, "ETH_TRANSFER_FAILED");
     }
 
-    /**
-     * @notice function to extract the selector of a bytes calldata
-     * @param _data the calldata bytes
-     */
+    /// @notice Function to extract the selector of a bytes calldata.
+    /// @param _data The calldata bytes.
     function getSelector(bytes memory _data) internal pure returns (bytes4 sig) {
         assembly {
             sig := mload(add(_data, 32))
+        }
+    }
+
+    function didLastOptionalReturnCallSucceed(bool callStatus) private pure returns (bool success) {
+        assembly {
+            // Get how many bytes the call returned.
+            let returnDataSize := returndatasize()
+
+            // If the call reverted:
+            if iszero(callStatus) {
+                // Copy the revert message into memory.
+                returndatacopy(0, 0, returnDataSize)
+
+                // Revert with the same message.
+                revert(0, returnDataSize)
+            }
+
+            switch returnDataSize
+            case 32 {
+                // Copy the return data into memory.
+                returndatacopy(0, 0, returnDataSize)
+
+                // Set success to whether it returned true.
+                success := iszero(iszero(mload(0)))
+            }
+            case 0 {
+                // There was no return data.
+                success := 1
+            }
+            default {
+                // It returned some malformed input.
+                success := 0
+            }
         }
     }
 }
