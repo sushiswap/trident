a8a9dc5882ae95da8a890542dc1ea487f4db95bb
Matthew Lilley <hello@matthewlilley.com>
2022-01-08 15:51:27 +0000

    chore: flesh out trident router test blocks

---
+++ test/TridentRouter.test.ts
@@ -0,0 +1,77 @@
+describe("Constant Product Pool Factory", function () {
+  before(async function () {
+    //
+  });
+
+  beforeEach(async function () {
+    //
+  });
+
+  describe("#exactInputSingle", function () {
+    //
+  });
+
+  describe("#exactInput", function () {
+    //
+  });
+
+  describe("#exactInputLazy", function () {
+    //
+  });
+
+  describe("#exactInputSingleWithNativeToken", function () {
+    //
+  });
+
+  describe("#exactInputWithNativeToken", function () {
+    //
+  });
+
+  describe("#complexPath", function () {
+    //
+  });
+
+  describe("#addLiquidity", function () {
+    //
+  });
+
+  describe("#addLiquidityLazy", function () {
+    //
+  });
+
+  describe("#burnLiquidity", function () {
+    //
+  });
+
+  describe("#burnLiquiditySingle", function () {
+    //
+  });
+
+  describe("#tridentSwapCallback", function () {
+    //
+  });
+
+  describe("#tridentMintCallback", function () {
+    //
+  });
+
+  describe("#sweepBentoBoxToken", function () {
+    //
+  });
+
+  describe("#sweepNativeToken", function () {
+    //
+  });
+
+  describe("#refundETH", function () {
+    //
+  });
+
+  describe("#unwrapWETH", function () {
+    //
+  });
+
+  describe("#isWhiteListed", function () {
+    //
+  });
+});
