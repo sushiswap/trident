39d849712258b8ed8fd4493f2dcd8386f986ebd1
Matthew Lilley <hello@matthewlilley.com>
2022-01-09 07:11:21 +0000

    config: solcover

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,22 @@
 module.exports = {
-    skipFiles: ["interfaces", "examples", "flat", "mocks"],
+  skipFiles: [
+    "interfaces",
+    "examples",
+    "flat",
+    "libraries/DyDxMath",
+    "libraries/FullMath",
+    "libraries/MathUtils",
+    "libraries/SafeCast",
+    "libraries/SwapLib",
+    "libraries/TickMath",
+    "libraries/Ticks",
+    "libraries/UnsafeMath",
+    "migration",
+    "mocks",
+    "pool/concentrated",
+    "pool/franchised",
+    "pool/hybrid",
+    "pool/index",
+    "pool/TridentERC721",
+  ],
 };
