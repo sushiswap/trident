6df5faf28940768b0d53d22a37136a3e58832470
Matthew Lilley <hello@matthewlilley.com>
2022-01-09 09:48:49 +0000

    spec(certora): remove debug flag from sanityConstantProductPool.sh

--- spec/scripts/sanityConstantProductPool.sh
+++ spec/scripts/sanityConstantProductPool.sh
@@ -2,5 +2,4 @@ certoraRun contracts/pool/constant-product/ConstantProductPool.sol \
 	--verify ConstantProductPool:spec/sanity.spec \
 	--optimistic_loop --loop_iter 2 \
 	--packages @openzeppelin=$PWD/node_modules/@openzeppelin \
-	--staging --msg "Constant Product Pool" \
-	--debug
+	--staging --msg "Constant Product Pool"
