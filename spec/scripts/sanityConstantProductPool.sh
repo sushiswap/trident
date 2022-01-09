e216becd002382c001c47e9aa82538e00b7571ed
Matthew Lilley <hello@matthewlilley.com>
2022-01-09 09:15:44 +0000

    spec(certora): add debug flag to sanityConstantProductPool

--- spec/scripts/sanityConstantProductPool.sh
+++ spec/scripts/sanityConstantProductPool.sh
@@ -2,4 +2,5 @@ certoraRun contracts/pool/constant-product/ConstantProductPool.sol \
 	--verify ConstantProductPool:spec/sanity.spec \
 	--optimistic_loop --loop_iter 2 \
 	--packages @openzeppelin=$PWD/node_modules/@openzeppelin \
-	--staging --msg "Constant Product Pool"
+	--staging --msg "Constant Product Pool" \
+	--debug
