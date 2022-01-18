5f92f4a43b7e12b99e6cf31b84c8e751f756b765
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-17 22:00:54 -0500

    Update hardhat.config.ts
    bump compiler

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -288,7 +288,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.7",
+        version: "0.8.11",
         settings: {
           optimizer: {
             enabled: true,
