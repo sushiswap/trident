09c8c50fa09e27aa9e116359e7775429804851af
Matthew Lilley <hello@matthewlilley.com>
2022-03-07 20:23:10 +0000

    fix: dont mint weth9

--- contracts/mocks/ERC20Mock.sol
+++ contracts/mocks/ERC20Mock.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pragma solidity ^0.8.2;
+pragma solidity >= 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
--- contracts/mocks/WETH9Mock.sol
+++ contracts/mocks/WETH9Mock.sol
@@ -1,11 +1,11 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >= 0.8.2;
 
 import "./ERC20Mock.sol";
 
 contract WETH9 is ERC20Mock {
-    constructor() ERC20Mock("WETH9", "WETH9", type(uint256).max) {
+    constructor() ERC20Mock("WETH9", "WETH9", 0) {
         //
     }
 }
