866c4794dff7cb1d0c88e5226c8e37a2965aaff4
Matthew Lilley <hello@matthewlilley.com>
2022-03-08 03:24:52 +0000

    chore: remove balanceOfThis & withdrawFromWETH

--- contracts/RouterHelper.sol
+++ contracts/RouterHelper.sol
@@ -42,15 +42,6 @@ contract RouterHelper is TridentPermit, TridentBatchable {
         bento.setMasterContractApproval(msg.sender, address(this), true, v, r, s);
     }
 
-    /// @notice Provides gas-optimized balance check on this contract to avoid redundant extcodesize check in addition to returndatasize check.
-    /// @param token Address of ERC-20 token.
-    /// @return balance Token amount held by this contract.
-    function balanceOfThis(address token) internal view returns (uint256 balance) {
-        (bool success, bytes memory data) = token.staticcall(abi.encodeWithSelector(0x70a08231, address(this))); // @dev balanceOf(address).
-        require(success && data.length >= 32, "BALANCE_OF_FAILED");
-        balance = abi.decode(data, (uint256));
-    }
-
     /// @notice Provides 'safe' ERC-20 {transfer} for tokens that don't consistently return true/false.
     /// @param token Address of ERC-20 token.
     /// @param recipient Account to send tokens to.
@@ -77,13 +68,6 @@ contract RouterHelper is TridentPermit, TridentBatchable {
     ) internal {
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, sender, recipient, amount)); // @dev transferFrom(address,address,uint256).
         require(success && (data.length == 0 || abi.decode(data, (bool))), "TRANSFER_FROM_FAILED");
-    }
-
-    /// @notice Provides low-level `wETH` {withdraw}.
-    /// @param amount Token amount to unwrap into ETH.
-    function withdrawFromWETH(uint256 amount) internal {
-        (bool success, ) = wETH.call(abi.encodeWithSelector(0x2e1a7d4d, amount)); // @dev withdraw(uint256).
-        require(success, "WITHDRAW_FROM_WETH_FAILED");
     }
 
     /// @notice Provides 'safe' ETH transfer.
