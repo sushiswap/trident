2a0be0e9bc9e3d94b2edbc89357bf3f60eda48aa
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-08 18:40:55 -0500

    Update TridentRouter.sol
    include erc20 back into mix / cleanup safetransferlib

--- contracts/TridentRouter.sol
+++ contracts/TridentRouter.sol
@@ -2,16 +2,17 @@
 
 pragma solidity >=0.8.0;
 
+import {ERC20} from "@rari-capital/solmate/src/tokens/ERC20.sol";
 import {SafeTransferLib} from "@rari-capital/solmate/src/utils/SafeTransferLib.sol";
 
-import {SelfPermit} from "./abstract/SelfPermit.sol";
 import {Multicall} from "./abstract/Multicall.sol";
+import {SelfPermit} from "./abstract/SelfPermit.sol";
 
 import {IBentoBoxMinimal} from "./interfaces/IBentoBoxMinimal.sol";
-import {IWETH9} from "./interfaces/IWETH9.sol";
+import {IMasterDeployer} from "./interfaces/IMasterDeployer.sol";
 import {IPool} from "./interfaces/IPool.sol";
 import {ITridentRouter} from "./interfaces/ITridentRouter.sol";
-import {IMasterDeployer} from "./interfaces/IMasterDeployer.sol";
+import {IWETH9} from "./interfaces/IWETH9.sol";
 
 // Custom Errors
 error NotWethSender();
@@ -23,6 +24,8 @@ error InvalidPool();
 
 /// @notice Router contract that helps in swapping across Trident pools.
 contract TridentRouter is ITridentRouter, SelfPermit, Multicall {
+    using SafeTransferLib for address;
+    using SafeTransferLib for ERC20;
     /// @dev Used to ensure that `tridentSwapCallback` is called only by the authorized address.
     /// These are set when someone calls a flash swap and reset afterwards.
     address internal cachedMsgSender;
@@ -198,7 +201,7 @@ contract TridentRouter is ITridentRouter, SelfPermit, Multicall {
         IPool.TokenAmount[] memory minWithdrawals
     ) public {
         isWhiteListed(pool);
-        SafeTransferLib.safeTransferFrom(ERC20(pool), msg.sender, pool, liquidity);
+        ERC20(pool).safeTransferFrom(msg.sender, pool, liquidity);
         IPool.TokenAmount[] memory withdrawnLiquidity = IPool(pool).burn(data);
         for (uint256 i; i < minWithdrawals.length; i++) {
             uint256 j;
@@ -227,7 +230,7 @@ contract TridentRouter is ITridentRouter, SelfPermit, Multicall {
     ) public {
         isWhiteListed(pool);
         // Use 'liquidity = 0' for prefunding.
-        SafeTransferLib.safeTransferFrom(ERC20(pool), msg.sender, pool, liquidity);
+        ERC20(pool).safeTransferFrom(msg.sender, pool, liquidity);
         uint256 withdrawn = IPool(pool).burnSingle(data);
         if (withdrawn < minWithdrawal) revert TooLittleReceived();
     }
@@ -242,7 +245,7 @@ contract TridentRouter is ITridentRouter, SelfPermit, Multicall {
         if (onBento) {
             bento.transfer(token, address(this), recipient, amount);
         } else {
-            token == USE_NATIVE ? SafeTransferLib.safeTransferETH(recipient, address(this).balance) : SafeTransferLib.safeTransfer(ERC20(token), recipient, amount);
+            token == USE_NATIVE ? recipient.safeTransferETH(address(this).balance) : ERC20(token).safeTransfer(recipient, amount);
         }
     }
 
@@ -252,7 +255,7 @@ contract TridentRouter is ITridentRouter, SelfPermit, Multicall {
         if (balance < amountMinimum) revert InsufficientWETH();
         if (balance != 0) {
             IWETH9(wETH).withdraw(balance);
-            SafeTransferLib.safeTransferETH(recipient, balance);
+            recipient.safeTransferETH(balance);
         }
     }
 
