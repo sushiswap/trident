48e65a3b67ecafdc60a457611a6c31bcd71d4d78
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-09 17:26:00 -0500

    Create SafeTransferLib.sol
    add univ2 safeTransferLib with solmate ETH helper for immediate purposes

---
+++ contracts/libraries/SafeTransferLib.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.8.4;
+
+/// @notice Helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true/false.
+/// @author Modified from Uniswap (https://github.com/Uniswap/solidity-lib/blob/master/contracts/libraries/TransferHelper.sol)
+/// and Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/SafeTransferLib.sol)
+library SafeTransferLib {
+    function safeTransfer(
+        address token,
+        address to,
+        uint256 value
+    ) internal {
+        // bytes4(keccak256(bytes('transfer(address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            'TransferHelper::safeTransfer: transfer failed'
+        );
+    }
+
+    function safeTransferFrom(
+        address token,
+        address from,
+        address to,
+        uint256 value
+    ) internal {
+        // bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            'TransferHelper::transferFrom: transferFrom failed'
+        );
+    }
+
+    function safeTransferETH(address to, uint256 amount) internal {
+        bool callStatus;
+
+        assembly {
+            // Transfer the ETH and store if it succeeded or not.
+            callStatus := call(gas(), to, amount, 0, 0, 0, 0)
+        }
+
+        require(callStatus, "ETH_TRANSFER_FAILED");
+    }
+}
