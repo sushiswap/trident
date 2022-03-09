a2e409b0de0e4974bc1ab652477f26fdf463b5d1
Matthew Lilley <hello@matthewlilley.com>
2022-03-09 22:42:09 +0000

    Update ConstantProductPoolFactory.sol

--- contracts/pool/constant-product/ConstantProductPoolFactory.sol
+++ contracts/pool/constant-product/ConstantProductPoolFactory.sol
@@ -9,7 +9,7 @@ import {ConstantProductPool} from "./ConstantProductPool.sol";
 /// @notice Contract for deploying Trident exchange Constant Product Pool with configurations.
 /// @author Mudit Gupta.
 contract ConstantProductPoolFactory is PoolDeployer {
-    constructor(IMasterDeployer _masterDeployer) PoolDeployer(_masterDeployer) {}
+    constructor(address _masterDeployer) PoolDeployer(_masterDeployer) {}
 
     function deployPool(bytes memory _deployData) public returns (address pool) {
         (address tokenA, address tokenB, uint256 swapFee, bool twapSupport) = abi.decode(_deployData, (address, address, uint256, bool));
@@ -27,7 +27,7 @@ contract ConstantProductPoolFactory is PoolDeployer {
 
         // Salt is not actually needed since `_deployData` is part of creationCode and already contains the salt.
         bytes32 salt = keccak256(_deployData);
-        pool = address(new ConstantProductPool{salt: salt}(_deployData, masterDeployer));
+        pool = address(new ConstantProductPool{salt: salt}(_deployData, IMasterDeployer(masterDeployer)));
         _registerPool(pool, tokens, salt);
     }
 }
