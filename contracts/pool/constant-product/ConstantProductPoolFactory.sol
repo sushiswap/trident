268f83454328b4d502dbb19e55f3e59419ee6d18
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-08 22:57:20 -0500

    Update ConstantProductPoolFactory.sol
    cleanups

--- contracts/pool/constant-product/ConstantProductPoolFactory.sol
+++ contracts/pool/constant-product/ConstantProductPoolFactory.sol
@@ -2,31 +2,31 @@
 
 pragma solidity >=0.8.0;
 
-import "./ConstantProductPool.sol";
-import "../../abstract/PoolDeployer.sol";
+import {ConstantProductPool} from "./ConstantProductPool.sol";
+import {PoolDeployer} "../../abstract/PoolDeployer.sol";
 
 /// @notice Contract for deploying Trident exchange Constant Product Pool with configurations.
 /// @author Mudit Gupta.
 contract ConstantProductPoolFactory is PoolDeployer {
     constructor(address _masterDeployer) PoolDeployer(_masterDeployer) {}
 
-    function deployPool(bytes memory _deployData) external returns (address pool) {
+    function deployPool(bytes calldata _deployData) public returns (address pool) {
         (address tokenA, address tokenB, uint256 swapFee, bool twapSupport) = abi.decode(_deployData, (address, address, uint256, bool));
 
         if (tokenA > tokenB) {
             (tokenA, tokenB) = (tokenB, tokenA);
         }
 
-        // @dev Strips any extra data.
+        // Strips any extra data.
         _deployData = abi.encode(tokenA, tokenB, swapFee, twapSupport);
 
         address[] memory tokens = new address[](2);
         tokens[0] = tokenA;
         tokens[1] = tokenB;
 
-        // @dev Salt is not actually needed since `_deployData` is part of creationCode and already contains the salt.
+        // Salt is not actually needed since `_deployData` is part of creationCode and already contains the salt.
         bytes32 salt = keccak256(_deployData);
-        pool = address(new ConstantProductPool{salt: salt}(_deployData, masterDeployer));
+        pool = address(new ConstantProductPool{salt: salt}(_deployData, IMasterDeployer(masterDeployer)));
         _registerPool(pool, tokens, salt);
     }
 }
