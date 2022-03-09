53e4688d9502c420322300f58d04561998933a57
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-09 18:33:53 -0500

    Update hardhat.config.ts
    bump sol compiler to 0.8.12

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -296,7 +296,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.7",
+        version: "0.8.12",
         settings: {
           optimizer: {
             enabled: true,
