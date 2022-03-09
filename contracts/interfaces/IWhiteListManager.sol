2634e9b7b870b29860fc1b4df5b8bfe200a5b2c4
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-09 17:38:55 -0500

    Update IWhiteListManager.sol

--- contracts/interfaces/IWhiteListManager.sol
+++ contracts/interfaces/IWhiteListManager.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pragma solidity >=0.8.0;
+pragma solidity >=0.8.4;
 
 /// @notice Trident franchised pool whitelist manager interface.
 interface IWhiteListManager {
