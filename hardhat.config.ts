b3b35ba734097edbe89e191e37d1f263abb3d293
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-05-31 00:24:57 +0530

    Update hardhat.config.ts
    bump sol thresh to 0.8.13. Good enough for Open Seaport

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -303,7 +303,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.10",
+        version: "0.8.13",
         settings: {
           optimizer: {
             enabled: true,
