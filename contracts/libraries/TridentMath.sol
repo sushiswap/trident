efc02112420d4a14fe1c24c0d3d897ea39770ee1
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-07-06 15:29:42 -0400

    Update TridentMath.sol
    comment clarifications

--- contracts/libraries/TridentMath.sol
+++ contracts/libraries/TridentMath.sol
@@ -2,23 +2,20 @@
 
 pragma solidity >=0.8.0;
 
-/// @notice Trident sqrt helper library.
+/// @notice Trident math library.
+/// @dev Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/FixedPointMathLib.sol)
 library TridentMath {
-    /// @dev Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/FixedPointMathLib.sol)
     function sqrt(uint256 x) internal pure returns (uint256 z) {
         assembly {
-            // This segment is to get a reasonable initial estimate for the Babylonian method.
-            // If the initial estimate is bad, the number of correct bits increases ~linearly
-            // each iteration instead of ~quadratically.
-            // The idea is to get z*z*y within a small factor of x.
-            // More iterations here gets y in a tighter range. Currently, we will have
-            // y in [256, 256*2^16). We ensure y>= 256 so that the relative difference
-            // between y and y+1 is small. If x < 256 this is not possible, but those cases
-            // are easy enough to verify exhaustively.
-            z := 181 // The 'correct' value is 1, but this saves a multiply later
-            let y := x
-            // Note that we check y>= 2^(k + 8) but shift right by k bits each branch,
-            // this is to ensure that if x >= 256, then y >= 256.
+            let y := x // We start y at x, which will help us make our initial estimate.
+
+            z := 181 // The "correct" value is 1, but this saves a multiplication later.
+
+            // This segment is to get a reasonable initial estimate for the Babylonian method. With a bad
+            // start, the correct # of bits increases ~linearly each iteration instead of ~quadratically.
+
+            // We check y >= 2^(k + 8) but shift right by k bits
+            // each branch to ensure that if x >= 256, then y >= 256.
             if iszero(lt(y, 0x10000000000000000000000000000000000)) {
                 y := shr(128, y)
                 z := shl(64, z)
@@ -35,21 +32,26 @@ library TridentMath {
                 y := shr(16, y)
                 z := shl(8, z)
             }
-            // Now, z*z*y <= x < z*z*(y+1), and y <= 2^(16+8),
-            // and either y >= 256, or x < 256.
+
+            // Goal was to get z*z*y within a small factor of x. More iterations could
+            // get y in a tighter range. Currently, we will have y in [256, 256*2^16).
+            // We ensured y >= 256 so that the relative difference between y and y+1 is small.
+            // That's not possible if x < 256 but we can just verify those cases exhaustively.
+
+            // Now, z*z*y <= x < z*z*(y+1), and y <= 2^(16+8), and either y >= 256, or x < 256.
             // Correctness can be checked exhaustively for x < 256, so we assume y >= 256.
             // Then z*sqrt(y) is within sqrt(257)/sqrt(256) of sqrt(x), or about 20bps.
 
-            // For s in the range [1/256, 256], the estimate f(s) = (181/1024) * (s+1)
-            // is in the range (1/2.84 * sqrt(s), 2.84 * sqrt(s)), with largest error when s=1
-            // and when s = 256 or 1/256. Since y is in [256, 256*2^16), let a = y/65536, so
-            // that a is in [1/256, 256). Then we can estimate sqrt(y) as
-            // sqrt(65536) * 181/1024 * (a + 1) = 181/4 * (y + 65536)/65536 = 181 * (y + 65536)/2^18
+            // For s in the range [1/256, 256], the estimate f(s) = (181/1024) * (s+1) is in the range
+            // (1/2.84 * sqrt(s), 2.84 * sqrt(s)), with largest error when s = 1 and when s = 256 or 1/256.
+
+            // Since y is in [256, 256*2^16), let a = y/65536, so that a is in [1/256, 256). Then we can estimate
+            // sqrt(y) using sqrt(65536) * 181/1024 * (a + 1) = 181/4 * (y + 65536)/65536 = 181 * (y + 65536)/2^18.
+
             // There is no overflow risk here since y < 2^136 after the first branch above.
-            z := shr(18, mul(z, add(y, 65536))) // A multiply is saved from the initial z := 181
+            z := shr(18, mul(z, add(y, 65536))) // A mul() is saved from starting z at 181.
 
             // Given the worst case multiplicative error of 2.84 above, 7 iterations should be enough.
-            // Possibly with a quadratic/cubic polynomial above we could get 4-6.
             z := shr(1, add(z, div(x, z)))
             z := shr(1, add(z, div(x, z)))
             z := shr(1, add(z, div(x, z)))
